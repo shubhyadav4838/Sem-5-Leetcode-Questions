aa53a620ed6b3a00b9e0c90155f03a29f1c8c90e
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-03 23:58:06 +0530

    Create 451-SortCharactersByFrequency

---
+++ 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
@@ -0,0 +1,24 @@
+// Last updated: 3/8/2025, 11:58:05 pm
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
+            (a, b) -> b.getValue() - a.getValue()
+        );
+        
+        pq.addAll(map.entrySet());
+        
+        StringBuilder result = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Map.Entry<Character, Integer> entry = pq.poll();
+            result.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+        }
+        return result.toString();
+    }
+}
+
