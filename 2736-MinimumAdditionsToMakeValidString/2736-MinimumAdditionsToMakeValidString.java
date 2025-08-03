54cb5d5fa08f8672aa3f5c8e377dfd837c87731f
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-03 23:53:48 +0530

    Create 2736-MinimumAdditionsToMakeValidString

---
+++ 2736-MinimumAdditionsToMakeValidString/2736-MinimumAdditionsToMakeValidString.java
@@ -0,0 +1,29 @@
+// Last updated: 3/8/2025, 11:53:47 pm
+public class Solution {
+    public int addMinimum(String word) {
+        int n = word.length(), i = 0, res = 0;
+        
+        while(i < n) {
+            int count = 0;
+            
+            if(word.charAt(i) == 'a') {
+                count++;
+                i++;
+            }
+             
+            if(i < n && word.charAt(i) == 'b') {
+                count++;
+                i++;
+            }
+            
+            if(i < n && word.charAt(i) == 'c') {
+                count++;
+                i++;
+            }
+            
+            res += 3 - count;
+        }
+        
+        return res;
+    }
+}
