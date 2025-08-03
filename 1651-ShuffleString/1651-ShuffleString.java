1842629bdbe286f6c38c166198f55498cbcb2688
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-03 23:54:49 +0530

    Create 1651-ShuffleString

---
+++ 1651-ShuffleString/1651-ShuffleString.java
@@ -0,0 +1,10 @@
+// Last updated: 3/8/2025, 11:54:48 pm
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] result=new char[s.length()];
+        for(int i=0;i<s.length();i++){
+            result[indices[i]]=s.charAt(i);
+        }
+        return new String(result);
+    }
+}
