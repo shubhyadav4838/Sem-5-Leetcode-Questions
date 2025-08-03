a8730a72352dccaa6409c3fed838ee6b7d5cb9d4
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-03 23:54:43 +0530

    Create 1720-CrawlerLogFolder

---
+++ 1720-CrawlerLogFolder/1720-CrawlerLogFolder.java
@@ -0,0 +1,19 @@
+// Last updated: 3/8/2025, 11:54:42 pm
+class Solution {
+    public int minOperations(String[] logs) {
+       
+        int res = 0;
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (res > 0) {
+                    res--;
+                }
+            } else if (!log.equals("./")) {
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
