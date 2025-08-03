b3f62bcc0e2501f033c7892453b582826d2ff906
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-03 23:54:27 +0530

    Create 1940-MaximumXorForEachQuery

---
+++ 1940-MaximumXorForEachQuery/1940-MaximumXorForEachQuery.java
@@ -0,0 +1,17 @@
+// Last updated: 3/8/2025, 11:54:27 pm
+class Solution {
+    public int[] getMaximumXor(int[] nums, int maximumBit) {
+        int mask = (1 << maximumBit) - 1;
+        int n = nums.length;
+        int[] res = new int[n];
+        int curr = 0;
+        
+        for(int i = 0; i < n; i++) {
+            curr ^= nums[i];
+            res[n-i-1] = ~curr & mask;
+        }
+        
+        return res;
+        
+    }
+}
