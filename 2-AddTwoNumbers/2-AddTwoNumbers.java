488e54f17eaa974c2106a58a85c066f9f44dc928
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-04 00:02:30 +0530

    Create 2-AddTwoNumbers

---
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -0,0 +1,32 @@
+// Last updated: 4/8/2025, 12:02:30 am
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry=0;
+        ListNode l3=new ListNode();
+        ListNode current=l3;
+        while (l1 != null || l2 != null || carry > 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next; }
+            if (l2 != null) {
+                sum += l2.val; 
+                l2 = l2.next; }
+
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+        return l3.next;
+    }
+}
