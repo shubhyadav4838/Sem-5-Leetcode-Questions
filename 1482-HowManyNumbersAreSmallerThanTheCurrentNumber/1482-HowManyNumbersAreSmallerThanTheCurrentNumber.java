959caf6df4b82aa452d1dbcbe933337b2c9d8fb9
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-03 23:55:04 +0530

    Create 1482-HowManyNumbersAreSmallerThanTheCurrentNumber

---
+++ 1482-HowManyNumbersAreSmallerThanTheCurrentNumber/1482-HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -0,0 +1,47 @@
+// Last updated: 3/8/2025, 11:55:03 pm
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n=nums.length;
+        int[] result=new int[n];
+        int[] countarr=new int[101];
+        for(int i=0;i<n;i++){
+            countarr[nums[i]]++;
+        }
+        int temp=0;
+        int c=0;
+        for(int i=0;i<101;i++){
+            if(countarr[i]>0){
+                temp=countarr[i];
+                countarr[i]=c;
+                c+=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            result[i]=countarr[nums[i]];
+        }
+        
+        return result;
+        
+    }
+}
+
+/*int n = nums.length;
+        int[] result = new int[n];
+        int[] countarr = new int[101];
+        for (int i = 0; i < n; i++) {
+            countarr[nums[i]]++;
+        }
+        int c = 0;
+        int temp = 0;
+        for (int i = 0; i < 101; i++) {
+            if (countarr[i] > 0) {
+                temp = countarr[i];
+                countarr[i] = c;
+                c += temp;
+        }
+        for (int i = 0; i < n; i++) {
+            result[i] = countarr[nums[i]];
+        }
+        return result;*/
+
+
