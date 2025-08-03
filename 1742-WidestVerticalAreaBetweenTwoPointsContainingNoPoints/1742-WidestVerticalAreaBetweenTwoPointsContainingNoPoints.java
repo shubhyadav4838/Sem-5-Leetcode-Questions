6146e837a950f397d43291bfb9ddffc10e1d8577
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-03 23:54:41 +0530

    Create 1742-WidestVerticalAreaBetweenTwoPointsContainingNoPoints

---
+++ 1742-WidestVerticalAreaBetweenTwoPointsContainingNoPoints/1742-WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java
@@ -0,0 +1,19 @@
+// Last updated: 3/8/2025, 11:54:41 pm
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        int[] arr=new int[points.length];
+        int max=0;
+        for(int i=0;i<points.length;i++){
+            arr[i]=points[i][0];
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i+1]-arr[i]>max){
+            max=arr[i+1]-arr[i];
+            }
+            
+        }
+        return max;
+       
+    }
+}
