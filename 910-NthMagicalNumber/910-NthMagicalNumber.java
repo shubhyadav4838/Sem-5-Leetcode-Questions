076519f11c30de416fc90f0dc2f6fb7192ddeb20
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-03 23:56:22 +0530

    Create 910-NthMagicalNumber

---
+++ 910-NthMagicalNumber/910-NthMagicalNumber.java
@@ -0,0 +1,28 @@
+// Last updated: 3/8/2025, 11:56:21 pm
+class Solution {
+    public int nthMagicalNumber(int n, int a, int b) {
+        
+        long A = a, B = b;
+        long mod = (long)(Math.pow(10,9)+7);
+        
+        
+        long left = Math.min(a,b), right = (long)n*Math.min(a,b);
+        
+        while (B > 0) {
+            long tmp = A;
+            A = B;
+            B = tmp % B;
+        }
+		
+        long lcm = (a*b)/A;  
+		
+        while (left < right) {
+            long m = left+(right-left)/2;
+			
+            if ((m / a) + (m / b) - (m / lcm) < n) left = m + 1;   
+			
+            else right = m;
+        }
+        return (int)(left % mod);
+    }
+}
