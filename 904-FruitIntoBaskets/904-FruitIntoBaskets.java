63cbda81981601b9df46673b29fa171f81150a80
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-04 23:29:34 +0530

    Create 904-FruitIntoBaskets

---
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.java
@@ -0,0 +1,36 @@
+// Last updated: 4/8/2025, 11:29:33 pm
+class Solution {
+    public int totalFruit(int[] fruits) {
+        return CountFruit(fruits);
+    }
+
+    public static int CountFruit(int[] fruits) {
+        if (fruits.length <= 2) {
+            return fruits.length;
+        }
+
+        int i = 0;
+        int ans = 0;
+        int lastFruit = -1, secondLastFruit = -1;
+        int lastFruitCount = 0; 
+        for (int j = 0; j < fruits.length; j++) {
+            if (fruits[j] == lastFruit || fruits[j] == secondLastFruit) {
+            } else {
+                i = j - lastFruitCount;
+                secondLastFruit = lastFruit;
+            }
+
+            if (fruits[j] == lastFruit) {
+                lastFruitCount++;
+            } else {
+                lastFruitCount = 1;
+                secondLastFruit = lastFruit;
+                lastFruit = fruits[j];
+            }
+
+            ans = Math.max(ans, j - i + 1);
+        }
+
+        return ans;
+    }
+}
