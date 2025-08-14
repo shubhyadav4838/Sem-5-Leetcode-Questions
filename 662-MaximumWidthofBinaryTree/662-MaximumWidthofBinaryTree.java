1b12ca2184f3b3352fe59acb1902db9ade48f94b
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-14 11:11:04 +0530

    Create 662-MaximumWidthofBinaryTree

---
+++ 662-MaximumWidthofBinaryTree/662-MaximumWidthofBinaryTree.java
@@ -0,0 +1,49 @@
+// Last updated: 14/8/2025, 11:11:04 am
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+		public int widthOfBinaryTree(TreeNode root) {
+			Queue<Pair> q = new LinkedList<>();
+			int width = 0;
+			q.add(new Pair(root,0));
+			while(!q.isEmpty()) {
+				Pair start = q.peek();
+				int s = q.size();
+				Pair end = null;
+				for(int i=0;i<s;i++) {
+					end = q.poll();
+					if(end.node.left!=null) {
+						q.add(new Pair(end.node.left,2*end.idx+1));
+					}
+					if(end.node.right!=null) {
+						q.add(new Pair(end.node.right,2*end.idx+2));
+					}
+				}
+				width = Math.max(width, end.idx-start.idx+1);
+			}
+			return width;
+
+		}
+		
+	}
+	class Pair{
+		TreeNode node;
+		int idx;
+		public Pair(TreeNode node, int idx) {
+			this.idx = idx;
+			this.node = node;
+		}
+}
