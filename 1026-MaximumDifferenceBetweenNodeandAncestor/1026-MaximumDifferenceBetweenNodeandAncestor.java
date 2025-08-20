d7e7798bf95d9c5c82f1c44ce3f4a666ed554278
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-21 00:24:00 +0530

    File updated at 21/8/2025, 12:24:00 am

--- 1026-MaximumDifferenceBetweenNodeandAncestor/1026-MaximumDifferenceBetweenNodeandAncestor.java
+++ 1026-MaximumDifferenceBetweenNodeandAncestor/1026-MaximumDifferenceBetweenNodeandAncestor.java
@@ -1,32 +1,27 @@
-// Last updated: 21/8/2025, 12:23:24 am
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Last updated: 21/8/2025, 12:24:00 am
+
+
 class Solution {
-    int ans = Integer.MIN_VALUE;
-    public int maxAncestorDiff(TreeNode root) {
-        difference(root,root.val,root.val);
-        return ans;
-        
-    }
-    public void difference(TreeNode root,int max,int min){
-        if(root==null)return;
-        int a = root.val-max;
-        int b = root.val-min;
-        ans = Math.max(ans,Math.max(Math.abs(a),Math.abs(b)));
-        difference(root.left,Math.max(max,root.val),Math.min(min,root.val));
-        difference(root.right,Math.max(max,root.val),Math.min(min,root.val));
+    public boolean isCompleteTree(TreeNode root) {
+        if (root == null) return true;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean seenNull = false;
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+
+            if (node == null) {
+                seenNull = true;
+            } else {
+                if (seenNull) {
+                    return false;
+                }
+                q.add(node.left);
+                q.add(node.right);
+            }
+        }
+        return true;
     }
 }
