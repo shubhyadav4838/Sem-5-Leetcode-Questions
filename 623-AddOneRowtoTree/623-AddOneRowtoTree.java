f9ae038af1c39bad41e2954fe62655637c3fa1b8
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-08-27 23:28:16 +0530

    File updated at 27/8/2025, 11:28:15 pm

--- 623-AddOneRowtoTree/623-AddOneRowtoTree.java
+++ 623-AddOneRowtoTree/623-AddOneRowtoTree.java
@@ -1,56 +1,33 @@
-// Last updated: 27/8/2025, 11:24:13 pm
+// Last updated: 27/8/2025, 11:28:15 pm
 /**
-
  * Definition for a binary tree node.
-
  * public class TreeNode {
-
  *     int val;
-
  *     TreeNode left;
-
  *     TreeNode right;
-
  *     TreeNode() {}
-
  *     TreeNode(int val) { this.val = val; }
-
  *     TreeNode(int val, TreeNode left, TreeNode right) {
-
  *         this.val = val;
-
  *         this.left = left;
-
  *         this.right = right;
-
  *     }
-
  * }
-
  */
-
 class Solution {
-
-    public TreeNode addOneRow(TreeNode root, int v, int d) {
-
-        if (d == 1) return new TreeNode(v, root, null);
-
-        else if (d == 2) {
-
-            root.left = new TreeNode(v, root.left, null);
-
-            root.right = new TreeNode(v, null, root.right);
-
-        } else {
-
-            if (root.left != null) addOneRow(root.left, v, d-1);
-
-            if (root.right != null) addOneRow(root.right, v, d-1);
-
-        }
-
-        return root;
-
+    public TreeNode bstToGst(TreeNode root) {
+        TreeNode dummy = root;
+        int a = change(root,0);
+        return dummy;
     }
-
+    public int change(TreeNode root, int sum){
+        if(root==null){
+            return sum;
+        }
+        int a = change(root.right,sum);
+        root.val +=a;
+        int b = change(root.left,root.val);
+        return b;
+    }
+    
 }
