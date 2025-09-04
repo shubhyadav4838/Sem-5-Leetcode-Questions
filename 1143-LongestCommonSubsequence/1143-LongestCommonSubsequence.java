7b68b5bb2e46b5400b5766bc21dcab3093701ac9
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-09-04 12:12:32 +0530

    File updated at 4/9/2025, 12:12:31 pm

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,23 +1,26 @@
-// Last updated: 4/9/2025, 11:17:19 am
+// Last updated: 4/9/2025, 12:12:31 pm
 class Solution {
-    public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        return LCSBU(nums1,nums2);
+    public int change(int amount, int[] coins) {
+        return coinChangeBU(coins,amount);
+        
     }
-    public static int LCSBU(int [] s1, int [] s2) {
-		int[][] dp = new int[s1.length+1][s2.length+1];
+    public static int coinChangeBU(int[] coin,int amount) {
+		int[][] dp = new int[coin.length+1][amount+1];
+		for(int i=0;i<dp.length;i++) { //row
+			dp[i][0] = 1;
+			
+		}
 		for(int i=1;i<dp.length;i++) {
-			for(int j = 1;j<dp[0].length;j++) {
-				int ans = 0;
-				if(s1[i-1] == s2[j-1] ) {
-					ans = 1+dp[i-1][j-1];
-				}else {
-					int f = dp[i-1][j];
-					int s = dp[i][j-1];
-					ans = Math.max(f, s);
+			for(int am = 1;am<dp[0].length;am++) {
+				int inc=0, exc = 0;
+				if(am>=coin[i-1]) {
+					inc = dp[i][am-coin[i-1]];
 				}
-				dp[i][j] = ans;
+				exc = dp[i-1][am];
+				dp[i][am] = inc+exc;
 			}
 		}
-		return dp[dp.length-1][dp[0].length-1];	
+		return dp[dp.length-1][dp[0].length-1];
+		
 	}
 }
