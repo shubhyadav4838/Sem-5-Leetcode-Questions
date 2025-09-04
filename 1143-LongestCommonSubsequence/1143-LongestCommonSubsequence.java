27c3fced4da99105f2fd11499efe5057f28172d8
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-09-04 12:27:10 +0530

    File updated at 4/9/2025, 12:27:09 pm

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,26 +1,29 @@
-// Last updated: 4/9/2025, 12:12:31 pm
+// Last updated: 4/9/2025, 12:27:09 pm
 class Solution {
-    public int change(int amount, int[] coins) {
-        return coinChangeBU(coins,amount);
-        
+    public int numDistinct(String s, String t) {
+        int[][]dp = new int[s.length()][t.length()];
+		for(int[] a:dp) {
+			Arrays.fill(a, -1);
+		}
+		return coinChange(s,t,0,0,dp);
     }
-    public static int coinChangeBU(int[] coin,int amount) {
-		int[][] dp = new int[coin.length+1][amount+1];
-		for(int i=0;i<dp.length;i++) { //row
-			dp[i][0] = 1;
-			
+    public static int coinChange(String s, String t, int i,int j,int[][] dp) {
+		if(j==t.length()) {
+			return 1;
 		}
-		for(int i=1;i<dp.length;i++) {
-			for(int am = 1;am<dp[0].length;am++) {
-				int inc=0, exc = 0;
-				if(am>=coin[i-1]) {
-					inc = dp[i][am-coin[i-1]];
-				}
-				exc = dp[i-1][am];
-				dp[i][am] = inc+exc;
-			}
+		if(i==s.length()) {
+			return 0;
 		}
-		return dp[dp.length-1][dp[0].length-1];
+		if(dp[i][j]!=-1) {
+			return dp[i][j];
+		}
+		
+		int inc = 0, exc = 0;
+		if(s.charAt(i)==t.charAt(j)) {
+			inc = coinChange(s,t,i+1,j+1,dp);
+		}
+		exc = coinChange(s,t,i+1,j,dp);
+		return dp[i][j] = inc+exc;
 		
 	}
 }
