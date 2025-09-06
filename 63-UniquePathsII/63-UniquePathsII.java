aa93ab4157e6e6eff8e3923145164821ecffbcf0
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-09-06 23:33:19 +0530

    File updated at 6/9/2025, 11:33:18 pm

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,26 +1,29 @@
-// Last updated: 6/9/2025, 11:11:26 pm
+// Last updated: 6/9/2025, 11:33:18 pm
 class Solution {
     public int uniquePathsWithObstacles(int[][] arr) {
-        if(arr[arr.length-1][arr[0].length-1]==1)return 0;
+        // if(arr[arr.length-1][arr[0].length-1]==1)return 0;
         int[][] dp = new int[arr.length][arr[0].length];
-        for(int[] x:dp){
-            Arrays.fill(x,-1);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i][0]==1){
+                break;
+            }
+            dp[i][0] = 1;
         }
-        return fun(0,0,arr,dp);
+        for(int i=0;i<arr[0].length;i++){
+            if(arr[0][i]==1){
+                break;
+            }
+            dp[0][i] = 1;
+        }
+        for(int r=1;r<arr.length;r++){
+            for(int c=1;c<arr[0].length;c++){
+                if(arr[r][c]==1)continue;
+                dp[r][c] = dp[r-1][c]+dp[r][c-1];
+            }
+        }
+       
+        return dp[arr.length-1][arr[0].length-1];
         
     }
-    public int fun(int r,int c,int[][] arr,int[][] dp){
-        if(r<0 || r>=arr.length || c<0 || c>=arr[0].length || arr[r][c]==1){
-            return 0;
-        }
-        if(r==arr.length-1 && c==arr[0].length-1){
-            return 1;
-        }
-        if(dp[r][c]!=-1){
-            return dp[r][c];
-        }
-        int a = fun(r+1,c,arr,dp);
-        int b = fun(r,c+1,arr,dp);
-        return dp[r][c] = a+b;
-    }
+   
 }
