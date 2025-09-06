bd77fb3c5e804ae31c373f2563dfd0c53aa6cbd5
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-09-06 23:11:26 +0530

    File updated at 6/9/2025, 11:11:26 pm

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,4 +1,4 @@
-// Last updated: 6/9/2025, 11:10:24 pm
+// Last updated: 6/9/2025, 11:11:26 pm
 class Solution {
     public int uniquePathsWithObstacles(int[][] arr) {
         if(arr[arr.length-1][arr[0].length-1]==1)return 0;
@@ -6,10 +6,10 @@ class Solution {
         for(int[] x:dp){
             Arrays.fill(x,-1);
         }
-        return fun(0,0,arr,0,dp);
+        return fun(0,0,arr,dp);
         
     }
-    public int fun(int r,int c,int[][] arr,int ans,int[][] dp){
+    public int fun(int r,int c,int[][] arr,int[][] dp){
         if(r<0 || r>=arr.length || c<0 || c>=arr[0].length || arr[r][c]==1){
             return 0;
         }
@@ -19,8 +19,8 @@ class Solution {
         if(dp[r][c]!=-1){
             return dp[r][c];
         }
-        int a = fun(r+1,c,arr,ans,dp);
-        int b = fun(r,c+1,arr,ans,dp);
+        int a = fun(r+1,c,arr,dp);
+        int b = fun(r,c+1,arr,dp);
         return dp[r][c] = a+b;
     }
 }
