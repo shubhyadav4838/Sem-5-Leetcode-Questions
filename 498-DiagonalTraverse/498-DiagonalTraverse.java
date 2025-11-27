39faaf80ca80142d3558fe92eebd88d5990bb86f
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-11-27 22:07:24 +0530

    File updated at 27/11/2025, 10:07:23 pm

--- 498-DiagonalTraverse/498-DiagonalTraverse.java
+++ 498-DiagonalTraverse/498-DiagonalTraverse.java
@@ -1,41 +1,25 @@
-// Last updated: 27/11/2025, 8:05:39 pm
-1class Solution {
-2    public int[] findDiagonalOrder(int[][] mat) {
-3        if (mat.length == 0 || mat[0].length == 0) return new int[0];
-4
-5        int m = mat.length, n = mat[0].length;
-6        int[] arr = new int[m * n];
-7        int i = 0, row = 0, col = 0;
-8        boolean flag = true;
-9
-10        while (i < m * n) {
-11            arr[i++] = mat[row][col];
-12
-13            if (flag) {
-14                if (col == n - 1) {
-15                    row++;
-16                    flag = false;
-17                } else if (row == 0) {
-18                    col++;
-19                    flag = false;
-20                } else {
-21                    row--;
-22                    col++;
-23                }
-24            } else {
-25                if (row == m - 1) {
-26                    col++;
-27                    flag = true;
-28                } else if (col == 0) {
-29                    row++;
-30                    flag = true;
-31                } else {
-32                    row++;
-33                    col--;
-34                }
-35            }
-36        }
-37
-38        return arr;
-39    }
-40}
+// Last updated: 27/11/2025, 10:07:23 pm
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m=mat.length;
+        int n= mat[0].length;
+        int[] res = new int[m*n];
+        int r=0,c=0;
+
+        for(int i=0;i<m*n;i++){
+            res[i]=mat[r][c];
+
+            if((r+c)%2==0){
+                if(c==n-1) r++;
+                else if(r==0) c++;
+                else {r--;c++;}
+            }
+            else{
+                if(r==m-1) c++;
+                else if(c==0) r++;
+                else {r++;c--;}
+            }
+        }
+        return res;
+    }
+}
