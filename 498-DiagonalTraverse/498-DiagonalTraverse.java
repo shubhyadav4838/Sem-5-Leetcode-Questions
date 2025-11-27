bea958d349676c1faaedd11cd3d681e383d627d5
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-11-27 20:05:40 +0530

    File updated at 27/11/2025, 8:05:39 pm

--- 498-DiagonalTraverse/498-DiagonalTraverse.java
+++ 498-DiagonalTraverse/498-DiagonalTraverse.java
@@ -1,41 +1,41 @@
-// Last updated: 3/8/2025, 11:57:44 pm
-class Solution {
-    public int[] findDiagonalOrder(int[][] mat) {
-        if (mat.length == 0 || mat[0].length == 0) return new int[0];
-
-        int m = mat.length, n = mat[0].length;
-        int[] arr = new int[m * n];
-        int i = 0, row = 0, col = 0;
-        boolean flag = true;
-
-        while (i < m * n) {
-            arr[i++] = mat[row][col];
-
-            if (flag) {
-                if (col == n - 1) {
-                    row++;
-                    flag = false;
-                } else if (row == 0) {
-                    col++;
-                    flag = false;
-                } else {
-                    row--;
-                    col++;
-                }
-            } else {
-                if (row == m - 1) {
-                    col++;
-                    flag = true;
-                } else if (col == 0) {
-                    row++;
-                    flag = true;
-                } else {
-                    row++;
-                    col--;
-                }
-            }
-        }
-
-        return arr;
-    }
-}
+// Last updated: 27/11/2025, 8:05:39 pm
+1class Solution {
+2    public int[] findDiagonalOrder(int[][] mat) {
+3        if (mat.length == 0 || mat[0].length == 0) return new int[0];
+4
+5        int m = mat.length, n = mat[0].length;
+6        int[] arr = new int[m * n];
+7        int i = 0, row = 0, col = 0;
+8        boolean flag = true;
+9
+10        while (i < m * n) {
+11            arr[i++] = mat[row][col];
+12
+13            if (flag) {
+14                if (col == n - 1) {
+15                    row++;
+16                    flag = false;
+17                } else if (row == 0) {
+18                    col++;
+19                    flag = false;
+20                } else {
+21                    row--;
+22                    col++;
+23                }
+24            } else {
+25                if (row == m - 1) {
+26                    col++;
+27                    flag = true;
+28                } else if (col == 0) {
+29                    row++;
+30                    flag = true;
+31                } else {
+32                    row++;
+33                    col--;
+34                }
+35            }
+36        }
+37
+38        return arr;
+39    }
+40}
