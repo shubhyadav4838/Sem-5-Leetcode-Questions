4d0a6e2a4a94838e07190824d74f55f8392566ec
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-11-27 23:22:56 +0530

    File updated at 27/11/2025, 11:22:55 pm

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -1,21 +1,26 @@
-// Last updated: 27/11/2025, 11:10:20 pm
+// Last updated: 27/11/2025, 11:22:55 pm
 1class Solution {
-2    public int longestOnes(int[] s, int k) {
-3         int[] count = new int[2];
-4        int maxCount = 0, left = 0, ans = 0;
-5
-6        for (int right = 0; right < s.length; right++) {
-7            count[s[right]]++;
-8            maxCount = Math.max(maxCount, count[1]);
-9
-10            while ((right - left + 1) - maxCount > k) {
-11                count[s[left]]--;
-12                left++;
-13            }
-14
-15            ans = Math.max(ans, right - left + 1);
-16        }
-17
-18        return ans;
-19    }
-20}
+2    public int longestOnes(int[] nums, int k) {
+3        return consecutive(nums,k);
+4    }
+5    public static int consecutive(int[] arr, int k) {
+6    	int one = 0, zero = 0,left = 0;
+7    	int ans = 0;
+8    	for(int right = 0;right<arr.length;right++) {
+9    		if(arr[right]==1) {
+10    			one++;
+11    		}else {
+12    			zero++;
+13    		}
+14    		if(zero>k) {
+15    			while(zero>k && left <arr.length) {
+16    				if( arr[left]==0)zero--;
+17    				left++;
+18    			}
+19    		}
+20    		ans = Math.max(ans, right-left+1);
+21    	}
+22    	return ans;
+23    }
+24
+25}
