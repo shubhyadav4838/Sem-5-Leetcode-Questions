999644984982a0e62ae44c5693a3e08921422ffd
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-12-02 01:18:09 +0530

    File updated at 2/12/2025, 1:18:09 am

--- 134-GasStation/134-GasStation.java
+++ 134-GasStation/134-GasStation.java
@@ -1,27 +1,27 @@
-// Last updated: 4/8/2025, 12:00:16 am
-class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        return canComplete(gas,cost);
-        
-    }
-    public static int canComplete(int[]gas, int[]cost) {
-		int total = 0;
-		for (int i= 0; i<cost.length; i++) {
-			total += gas[i] - cost[i];
-		}
-		if(total<0) {
-			return -1;
-		}
-		int idx = 0;
-		int curr = 0;
-		for(int i=0; i<cost.length; i++) {
-			curr+= gas[i]-cost[i];
-			if(curr<0) {
-				curr = 0;
-				idx = i+1;
-			}
-			
-		}
-		return idx;
-	}
-}
+// Last updated: 2/12/2025, 1:18:09 am
+1class Solution {
+2    public int canCompleteCircuit(int[] gas, int[] cost) {
+3        return canComplete(gas,cost);
+4        
+5    }
+6    public static int canComplete(int[]gas, int[]cost) {
+7		int total = 0;
+8		for (int i= 0; i<cost.length; i++) {
+9			total += gas[i] - cost[i];
+10		}
+11		if(total<0) {
+12			return -1;
+13		}
+14		int idx = 0;
+15		int curr = 0;
+16		for(int i=0; i<cost.length; i++) {
+17			curr+= gas[i]-cost[i];
+18			if(curr<0) {
+19				curr = 0;
+20				idx = i+1;
+21			}
+22			
+23		}
+24		return idx;
+25	}
+26}
