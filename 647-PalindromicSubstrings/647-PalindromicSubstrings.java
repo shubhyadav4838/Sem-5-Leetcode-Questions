2a5c70a0e3d1e83b9d60e6ce341c75bfddc1609b
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-12-30 01:15:15 +0530

    File updated at 30/12/2025, 1:15:14 am

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.java
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -1,35 +1,29 @@
-// Last updated: 3/8/2025, 11:57:15 pm
-class Solution {
-    public int countSubstrings(String s) {
-        char[] charArray = s.toCharArray();
-        int n = charArray.length;
-
-        int left = 0;
-        int right = 0;
-
-        int result = 0;
-
-        for (int i = 0; i < n; i++) {
-            left = i - 1;
-            right = i;
-
-            while (right < n - 1 && charArray[right] == charArray[right + 1]) {
-                right++;
-            }
-
-            result += ((right - left) * (right - left + 1)) / 2;
-
-            i = right;
-
-            right++;
-
-            while (left >= 0 && right < n && charArray[left] == charArray[right]) {
-                result++;
-                left--;
-                right++;
-            }
-        }
-
-        return result;
-    }
-}
+// Last updated: 30/12/2025, 1:15:14 am
+1class Solution {
+2    public int countSubstrings(String s) {
+3        return countpalindrome(s);
+4    }
+5    public static int countpalindrome(String s) {
+6		int even = 0;
+7		int odd = s.length();
+8		int n = s.length();
+9		for(int i = 0;i<s.length();i++) {
+10			for(int orbit = 1;i-orbit>=0 && i+orbit<n;orbit++) {
+11				if(s.charAt(i-orbit)!=s.charAt(i+orbit)) {
+12					break;
+13				}
+14                odd++;
+15			}
+16		}
+17		for(double i = 0.5;i<s.length();i++) {
+18			for(double orbit = 0.5;i-orbit>=0 && i+orbit<n;orbit++) {
+19				if(s.charAt((int)(i-orbit))!=s.charAt((int)(i+orbit))) {
+20					break;
+21				}
+22                even++;
+23			}
+24		}
+25		return odd+even;
+26	}
+27
+28}
