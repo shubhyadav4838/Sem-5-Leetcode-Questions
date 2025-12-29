d94a48dbc1ebc54270da8a873e7644446df7faf6
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2025-12-30 01:21:22 +0530

    File updated at 30/12/2025, 1:21:20 am

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.java
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -1,29 +1,22 @@
-// Last updated: 30/12/2025, 1:15:14 am
+// Last updated: 30/12/2025, 1:21:20 am
 1class Solution {
 2    public int countSubstrings(String s) {
-3        return countpalindrome(s);
-4    }
-5    public static int countpalindrome(String s) {
-6		int even = 0;
-7		int odd = s.length();
-8		int n = s.length();
-9		for(int i = 0;i<s.length();i++) {
-10			for(int orbit = 1;i-orbit>=0 && i+orbit<n;orbit++) {
-11				if(s.charAt(i-orbit)!=s.charAt(i+orbit)) {
-12					break;
-13				}
-14                odd++;
-15			}
-16		}
-17		for(double i = 0.5;i<s.length();i++) {
-18			for(double orbit = 0.5;i-orbit>=0 && i+orbit<n;orbit++) {
-19				if(s.charAt((int)(i-orbit))!=s.charAt((int)(i+orbit))) {
-20					break;
-21				}
-22                even++;
-23			}
-24		}
-25		return odd+even;
-26	}
-27
-28}
+3        int n = s.length();
+4        boolean[][] dp = new boolean[n][n];
+5        int count = 0;
+6        for (int i = n - 1; i >= 0; i--) {
+7            for (int j = i; j < n; j++) {
+8                
+9                if (s.charAt(i) == s.charAt(j)) {
+10                    
+11                    if ((j - i < 2) || dp[i+1][j-1]) {
+12                        dp[i][j] = true;
+13                        count++;
+14                    }
+15                }
+16            }
+17        }
+18        
+19        return count;
+20    }
+21}
