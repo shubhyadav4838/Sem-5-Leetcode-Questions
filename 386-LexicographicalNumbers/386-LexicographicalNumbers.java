b00f30b248b44969bab33c306453f4ddf37f2a71
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-01 18:23:49 +0530

    File updated at 1/1/2026, 6:23:49 pm

--- 386-LexicographicalNumbers/386-LexicographicalNumbers.java
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.java
@@ -1,25 +1,22 @@
-// Last updated: 3/8/2025, 11:58:38 pm
-class Solution {
-    public List<Integer> lexicalOrder(int n) {
-        List<Integer> l = new ArrayList<Integer>();
-        print(n,0,l);
-        return l;
-
-    }
-    public static void print(int n, int curr, List<Integer> l) {
-		if(curr>n) {
-			return;
-		}
-		// System.out.println(curr);
-        
-		int i=0;
-		if(curr==0) {
-			i = 1;
-		}else{
-            l.add(curr);
-        }
-		for(; i<=9; i++) {
-			print(n, curr*10+i,l);	
-		}
-	}
-}
+// Last updated: 1/1/2026, 6:23:49 pm
+1class Solution {
+2    public List<Integer> lexicalOrder(int n) {
+3        ArrayList<Integer> ll = new ArrayList<>();
+4        order(0,n,ll);
+5        ll.remove(0);
+6        return ll;
+7    }
+8    public static void order(int cur, int n,ArrayList<Integer>ll){
+9        if(cur>n)return;
+10
+11        ll.add(cur);
+12        int i = 0;
+13        if(cur == 0){
+14            i = 1;
+15        }
+16
+17        for(;i<=9;i++){
+18            order(cur*10+i,n,ll);
+19        }
+20    }
+21}
