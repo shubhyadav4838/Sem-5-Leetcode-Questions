2b7028729f6582d24a3c978380e3b314fc0e0ec7
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-02 15:57:07 +0530

    File updated at 2/1/2026, 3:57:07 pm

--- 77-Combinations/77-Combinations.java
+++ 77-Combinations/77-Combinations.java
@@ -1,20 +1,24 @@
-// Last updated: 4/8/2025, 12:00:47 am
-class Solution {
-    public List<List<Integer>> combine(int n, int k) {
-        List<Integer>ll = new ArrayList<>();
-		List<List<Integer>> ans = new ArrayList<>();
-		combination(ll,ans,n,k,1);
-        return ans;
-    }
-    public static void combination(List<Integer>ll, List<List<Integer>> ans,int n, int k, int idx ) {
-		if(ll.size()==k) {
-			ans.add(new ArrayList<>(ll));
-			return;
-		}
-		for(int i=idx;i<=n;i++) {
-			ll.add(i);
-			combination(ll,ans,n,k,i+1);
-			ll.remove(ll.size()-1);
-		}
-	}
-}
+// Last updated: 2/1/2026, 3:57:07 pm
+1class Solution {
+2    public List<List<Integer>> combine(int n, int k) {
+3        List<Integer> ll = new ArrayList<>();
+4		List<List<Integer>> ans = new ArrayList<>();
+5		solve( n, 1, k, ll, ans);
+6        return ans;
+7    }
+8    private static void solve(int n, int cur, int k, List<Integer> ll, List<List<Integer>> ans) {
+9		// TODO Auto-generated method stub
+10		if(ll.size()>k)return;
+11		if(ll.size()==k) {
+12			ans.add(new ArrayList<>(ll));
+13			return;
+14		}
+15		
+16		for(int i = cur; i<=n;i++) {
+17			ll.add(i);
+18			solve(n,i+1,k,ll,ans);
+19			ll.remove(ll.size()-1);
+20		}
+21		
+22	}
+23}
