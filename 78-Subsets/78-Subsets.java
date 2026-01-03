4f6977886bf897809bf76b7c84071f51ad896fcb
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-03 10:55:41 +0530

    File updated at 3/1/2026, 10:55:40 am

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,21 +1,26 @@
-// Last updated: 4/8/2025, 12:00:46 am
-class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();// 2D list
-		List<Integer> ll =new ArrayList<>();
-		subset(ans,ll,0,nums);
-        return ans;
-    }
-    public static void subset(List<List<Integer>> ans, List<Integer> ll,int idx, int[]arr) {
-		if(ll.size()<= arr.length) {
-			ans.add(new ArrayList<>(ll));
-			
-		}
-		for(int i=idx;i<arr.length;i++) {
-			ll.add(arr[i]);
-			subset(ans,ll,i+1,arr);
-			ll.remove(ll.size()-1);
-		}
-		
-	}
-}
+// Last updated: 3/1/2026, 10:55:40 am
+1class Solution {
+2    public List<List<Integer>> subsets(int[] nums) {
+3        List<Integer> ll = new ArrayList<>();
+4		List<List<Integer>> ans = new ArrayList<>();
+5		
+6		solve(nums,0,ll,ans);
+7        return ans;
+8    }
+9    private static void solve(int[] arr, int idx, List<Integer> ll, List<List<Integer>> ans) {
+10		// TODO Auto-generated method stub
+11		ans.add(new ArrayList<>(ll));
+12		if(idx==arr.length) {
+13			return;
+14		}
+15		
+16		
+17		
+18		for(int i = idx; i<arr.length;i++) {
+19			ll.add(arr[i]);
+20			solve(arr,i+1,ll,ans);
+21			ll.remove(ll.size()-1);
+22		}
+23		
+24	}
+25}
