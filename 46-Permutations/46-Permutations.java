be6eca87f6af0d5e52979c191bfe81f36c784a65
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-03 13:46:13 +0530

    File updated at 3/1/2026, 1:46:11 pm

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,28 +1,29 @@
-// Last updated: 4/8/2025, 12:01:29 am
-class Solution {
-    public List<List<Integer>> permute(int[] arr) {
-        List<List<Integer>> ans = new ArrayList<>();
-		List<Integer> ll = new ArrayList();
-		boolean[] isvisited = new boolean[arr.length];
-		permutation(ans,ll,isvisited,arr);
-        return ans;
-        
-    }
-    public static void permutation(List<List<Integer>> ans,List<Integer> ll,boolean[] isvisited,int[] arr ) {
-		if(ll.size()==arr.length) {
-			ans.add(new ArrayList<>(ll));
-			return ;
-		}
-		for(int i=0;i<arr.length;i++) {
-			if(!isvisited[i]) {
-				isvisited[i]=true;
-				ll.add(arr[i]);
-				permutation(ans,ll,isvisited,arr);
-				isvisited[i]=false;
-				ll.remove((ll.size()-1));
-			}
-		}
-		
-		
-	}
-}
+// Last updated: 3/1/2026, 1:46:11 pm
+1class Solution {
+2    public List<List<Integer>> permute(int[] arr) {
+3        List<Integer> ll = new ArrayList<>();
+4		List<List<Integer>> ans = new ArrayList<>();
+5		boolean[] visited = new boolean[arr.length];
+6		solve(arr,ll,ans,visited);
+7        return ans;
+8    }
+9    private static void solve(int[] arr, List<Integer> ll, List<List<Integer>> ans,
+10			boolean[] visited) {
+11		// TODO Auto-generated method stub
+12		if(ll.size()==arr.length) {
+13			ans.add(new ArrayList<>(ll));
+14			return;
+15		}
+16		
+17		for(int i = 0;i<arr.length;i++) {
+18			if(!visited[i]) {
+19				visited[i] = true;
+20				ll.add(arr[i]);
+21				solve(arr,ll,ans,visited);
+22				ll.remove(ll.size()-1);
+23				visited[i] = false;
+24			}
+25		}
+26		
+27	}
+28}
