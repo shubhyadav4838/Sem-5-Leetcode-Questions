0b89231da02e603a3817b3bd1740e39887f5d98d
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-06 14:11:22 +0530

    File updated at 6/1/2026, 2:11:23 pm

--- 213-HouseRobberII/213-HouseRobberII.java
+++ 213-HouseRobberII/213-HouseRobberII.java
@@ -1,34 +1,22 @@
-// Last updated: 3/9/2025, 10:56:47 pm
-class Solution {
-    public int rob(int[] arr) {
-        if(arr.length==1){
-            return arr[0];
-        }
-        
-        int first = robHouse(arr,0,arr.length-1);
-        int second = robHouse(arr,1,arr.length);
-        return Math.max(first,second);
-        
-        
-    }
-    public int robHouse(int[] arr,int start, int end){
-        int[] dp = new int[arr.length];
-        if(start == 1){
-            arr[0] = 0;
-        }
-        dp[0] = arr[0];
-        dp[1] = Math.max(arr[0],arr[1]);
-        for(int i=2;i<end;i++){
-            int rob = dp[i-2]+arr[i];
-            int dont = dp[i-1];
-            dp[i] = Math.max(rob,dont);
-            
-        }
-        if(end==arr.length){
-            return dp[dp.length-1];
-        }
-        return dp[dp.length-2];
-
-    }
-    
-}
+// Last updated: 6/1/2026, 2:11:23 pm
+1class Solution {
+2    public int rob(int[] nums) {
+3        if(nums.length==1){
+4            return nums[0];
+5        }
+6        int a = robHouse(nums,0,nums.length-2);
+7        int b = robHouse(nums,1,nums.length-1);
+8        return Math.max(a,b);
+9        
+10    }
+11    public static int robHouse(int[] nums, int si, int ei){
+12        int ans = 0;
+13        int pre = 0;
+14        for(int i = si;i<=ei;i++){
+15            int temp = Math.max(ans,pre+nums[i]);
+16            pre = ans;
+17            ans = temp;
+18        }
+19        return ans;
+20    }
+21}
