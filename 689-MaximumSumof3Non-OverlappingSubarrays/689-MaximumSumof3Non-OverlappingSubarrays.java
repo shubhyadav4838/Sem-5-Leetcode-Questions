080f3508776ab36b2a5a5bdb62272fe38c85ea69
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-08 20:30:51 +0530

    File updated at 8/1/2026, 8:30:50 pm

--- 689-MaximumSumof3Non-OverlappingSubarrays/689-MaximumSumof3Non-OverlappingSubarrays.java
+++ 689-MaximumSumof3Non-OverlappingSubarrays/689-MaximumSumof3Non-OverlappingSubarrays.java
@@ -1,56 +1,55 @@
-// Last updated: 8/1/2026, 8:23:01 pm
+// Last updated: 8/1/2026, 8:30:50 pm
 1class Solution {
 2    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
-3        int n = nums.length;
-4
+3
+4        int n = nums.length;
 5        int[] sums = new int[n - k + 1];
-6        int currentSum = 0;
-7        for (int i = 0; i < n; i++) {
-8            currentSum += nums[i];
-9            if (i >= k) currentSum -= nums[i - k];
-10            if (i >= k - 1) sums[i - k + 1] = currentSum;
-11        }
-12
-13        int[][] dp = new int[4][sums.length];
-14
-15        for (int j = 1; j <= 3; j++) { 
-16            for (int i = 0; i < sums.length; i++) {
-17                int skip = (i > 0) ? dp[j][i - 1] : 0;
-18                int take = sums[i];
-19                if (j > 1) {
-20                    if (i >= k) {
-21                        take += dp[j - 1][i - k];
-22                    } else {
-23                        take = 0; 
-24                    }
-25                }
-26                
-27                dp[j][i] = Math.max(skip, take);
-28            }
-29        }
+6        int currentWindowSum = 0;
+7        
+8        for (int i = 0; i < nums.length; i++) {
+9            currentWindowSum += nums[i];
+10            if (i >= k) {
+11                currentWindowSum -= nums[i - k];
+12            }
+13            if (i >= k - 1) {
+14                sums[i - k + 1] = currentWindowSum;
+15            }
+16        }
+17
+18        
+19        int maxOne = 0;
+20        int bestIdxOne = 0;
+21
+22        
+23        int maxTwo = 0;
+24        int[] bestIdxTwo = {0, k}; 
+25
+26        
+27        int maxThree = 0;
+28        int[] bestIdxThree = {0, k, 2 * k}; 
+29
 30
-31        int[] ans = new int[3];
-32        int currIdx = sums.length - 1;
-33        
-34        for (int j = 3; j >= 1; j--) {
-35            while (currIdx > 0) {
-36                int takeVal = sums[currIdx];
-37                if (j > 1) {
-38                   if (currIdx - k >= 0) takeVal += dp[j - 1][currIdx - k];
-39                   else takeVal = -1;
-40                }
-41 
-42                if (dp[j][currIdx - 1] == dp[j][currIdx]) {
-43                    currIdx--;
-44                } else {
-45                    break; 
-46                }
-47            }
-48            
-49            ans[j - 1] = currIdx;
-50            currIdx = currIdx - k; 
-51        }
-52        
-53        return ans;
-54    }
-55}
+31        for (int i = 2 * k; i < sums.length; i++) {
+32            int idx1 = i - 2 * k; 
+33            int idx2 = i - k;     
+34            int idx3 = i;         
+35
+36            if (sums[idx1] > maxOne) {
+37                maxOne = sums[idx1];
+38                bestIdxOne = idx1;
+39            }
+40
+41            if (maxOne + sums[idx2] > maxTwo) {
+42                maxTwo = maxOne + sums[idx2];
+43                bestIdxTwo = new int[]{bestIdxOne, idx2};
+44            }
+45
+46            if (maxTwo + sums[idx3] > maxThree) {
+47                maxThree = maxTwo + sums[idx3];
+48                bestIdxThree = new int[]{bestIdxTwo[0], bestIdxTwo[1], idx3};
+49            }
+50        }
+51
+52        return bestIdxThree;
+53    }
+54}
