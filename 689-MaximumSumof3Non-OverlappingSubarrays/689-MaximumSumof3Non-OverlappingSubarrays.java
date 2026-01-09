0ff8359929913a7b38766daeafea74ebd2d17592
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-09 12:40:17 +0530

    File updated at 9/1/2026, 12:40:17 pm

--- 689-MaximumSumof3Non-OverlappingSubarrays/689-MaximumSumof3Non-OverlappingSubarrays.java
+++ 689-MaximumSumof3Non-OverlappingSubarrays/689-MaximumSumof3Non-OverlappingSubarrays.java
@@ -1,55 +1,54 @@
-// Last updated: 9/1/2026, 12:21:02 pm
+// Last updated: 9/1/2026, 12:40:17 pm
 1class Solution {
-2    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
-3
-4        int n = nums.length;
-5        int[] sums = new int[n - k + 1];
-6        int currentWindowSum = 0;
-7        
-8        for (int i = 0; i < nums.length; i++) {
-9            currentWindowSum += nums[i];
-10            if (i >= k) {
-11                currentWindowSum -= nums[i - k];
-12            }
-13            if (i >= k - 1) {
-14                sums[i - k + 1] = currentWindowSum;
-15            }
-16        }
-17
-18        
-19        int maxOne = 0;
-20        int bestIdxOne = 0;
-21
-22        
-23        int maxTwo = 0;
-24        int[] bestIdxTwo = {0, k}; 
-25
-26        
-27        int maxThree = 0;
-28        int[] bestIdxThree = {0, k, 2 * k}; 
-29
-30
-31        for (int i = 2 * k; i < sums.length; i++) {
-32            int idx1 = i - 2 * k; 
-33            int idx2 = i - k;     
-34            int idx3 = i;         
-35
-36            if (sums[idx1] > maxOne) {
-37                maxOne = sums[idx1];
-38                bestIdxOne = idx1;
-39            }
-40
-41            if (maxOne + sums[idx2] > maxTwo) {
-42                maxTwo = maxOne + sums[idx2];
-43                bestIdxTwo = new int[]{bestIdxOne, idx2};
-44            }
-45
-46            if (maxTwo + sums[idx3] > maxThree) {
-47                maxThree = maxTwo + sums[idx3];
-48                bestIdxThree = new int[]{bestIdxTwo[0], bestIdxTwo[1], idx3};
-49            }
-50        }
-51
-52        return bestIdxThree;
-53    }
-54}
+2   public static int[] maxSumOfThreeSubarrays(int[] arr, int k) {
+3		long[] pre = new long[arr.length];
+4		long sum = 0;
+5		for(int i = 0;i<pre.length;i++) {
+6			sum+=(long)arr[i];
+7			if(i==k-1) {
+8				pre[i] = sum;
+9			}
+10			else if(i>=k) {
+11				sum-= (long)arr[i-k];
+12				pre[i] = sum;
+13			}
+14		}
+15		
+16		for(long x:pre) {
+17			System.out.print(x+" ");
+18		}
+19		System.out.println();
+20		
+21		int first = 0;
+22		int[] second = {0,0};
+23		int[] third = {0,0,0};
+24		
+25		long firstarr = 0;
+26		long secarr = 0;
+27		long thirdarr = 0;
+28		
+29		
+30		for(int i = k*3-1; i<pre.length;i++) {
+31			int a = i-2*k;
+32			int b = i-k;
+33			int c = i;
+34			
+35			if(pre[a]>firstarr) {
+36				firstarr = pre[a];
+37				first = a-k+1;
+38			}
+39			
+40			if(firstarr+pre[b]>secarr) {
+41				secarr = firstarr+pre[b];
+42				second = new int[] {first,b-k+1};
+43			}
+44			
+45			if(secarr+pre[c]>thirdarr) {
+46				thirdarr = secarr+pre[c];
+47				third = new int[] {second[0],second[1],c-k+1};
+48			}
+49		}
+50		return third;
+51		
+52	}
+53}
