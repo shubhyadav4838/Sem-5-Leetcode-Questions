961d8264f5cd07b27275f6ba6a087ccec981cd78
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-09 12:40:44 +0530

    File updated at 9/1/2026, 12:40:44 pm

--- 689-MaximumSumof3Non-OverlappingSubarrays/689-MaximumSumof3Non-OverlappingSubarrays.java
+++ 689-MaximumSumof3Non-OverlappingSubarrays/689-MaximumSumof3Non-OverlappingSubarrays.java
@@ -1,4 +1,4 @@
-// Last updated: 9/1/2026, 12:40:17 pm
+// Last updated: 9/1/2026, 12:40:44 pm
 1class Solution {
 2   public static int[] maxSumOfThreeSubarrays(int[] arr, int k) {
 3		long[] pre = new long[arr.length];
@@ -14,41 +14,36 @@
 13			}
 14		}
 15		
-16		for(long x:pre) {
-17			System.out.print(x+" ");
-18		}
-19		System.out.println();
-20		
-21		int first = 0;
-22		int[] second = {0,0};
-23		int[] third = {0,0,0};
+16		int first = 0;
+17		int[] second = {0,0};
+18		int[] third = {0,0,0};
+19		
+20		long firstarr = 0;
+21		long secarr = 0;
+22		long thirdarr = 0;
+23		
 24		
-25		long firstarr = 0;
-26		long secarr = 0;
-27		long thirdarr = 0;
-28		
-29		
-30		for(int i = k*3-1; i<pre.length;i++) {
-31			int a = i-2*k;
-32			int b = i-k;
-33			int c = i;
+25		for(int i = k*3-1; i<pre.length;i++) {
+26			int a = i-2*k;
+27			int b = i-k;
+28			int c = i;
+29			
+30			if(pre[a]>firstarr) {
+31				firstarr = pre[a];
+32				first = a-k+1;
+33			}
 34			
-35			if(pre[a]>firstarr) {
-36				firstarr = pre[a];
-37				first = a-k+1;
+35			if(firstarr+pre[b]>secarr) {
+36				secarr = firstarr+pre[b];
+37				second = new int[] {first,b-k+1};
 38			}
 39			
-40			if(firstarr+pre[b]>secarr) {
-41				secarr = firstarr+pre[b];
-42				second = new int[] {first,b-k+1};
+40			if(secarr+pre[c]>thirdarr) {
+41				thirdarr = secarr+pre[c];
+42				third = new int[] {second[0],second[1],c-k+1};
 43			}
-44			
-45			if(secarr+pre[c]>thirdarr) {
-46				thirdarr = secarr+pre[c];
-47				third = new int[] {second[0],second[1],c-k+1};
-48			}
-49		}
-50		return third;
-51		
-52	}
-53}
+44		}
+45		return third;
+46		
+47	}
+48}
