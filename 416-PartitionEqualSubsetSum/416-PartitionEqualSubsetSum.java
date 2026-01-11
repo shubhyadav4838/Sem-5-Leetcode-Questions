e0f960b8ce999d52e9483139fb05d7a89f6c0511
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-12 01:09:54 +0530

    File updated at 12/1/2026, 1:09:53 am

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -1,34 +1,33 @@
-// Last updated: 12/1/2026, 12:23:40 am
+// Last updated: 12/1/2026, 1:09:53 am
 1class Solution {
-2    static Boolean[][] dp; 
-3	public static boolean canPartition(int[] arr) {
-4		int sum = 0;
-5		for(int x:arr) {
-6			sum+=x;
-7		}
-8		if(sum%2!=0) {
-9			return false;
-10		}
-11		int target = sum/2;
-12		dp = new Boolean[arr.length][target+1];
-13		return solve(0,0,target,arr,dp);
-14		
-15	}
-16	
-17	private static boolean solve(int sum, int idx, int target,int[] arr, Boolean[][] dp ) {
-18		// TODO Auto-generated method stub
-19		if(sum==target) {
-20			return true;
-21		}
-22		if(sum>target || idx==arr.length) {
-23			return false;
-24		}
-25		
-26		if(dp[idx][sum]!=null) {
-27			return dp[idx][sum];
+2   public static int coinChange(int[] arr, int amount) {
+3		int[] dp = new int[amount+1];
+4		Arrays.fill(dp, -1);
+5        int ans = solve(amount,arr,dp); 
+6		return (ans==999999999) ? -1:ans;
+7	}
+8	
+9	public static int solve(int target, int[] arr,int[] dp) {
+10		if(target==0) {
+11			return 0;
+12		}
+13		if(target<0) {
+14			return 999999999;
+15		}
+16		
+17		if(dp[target]!=-1) {
+18			return dp[target];
+19		}
+20		
+21		int ans = 999999999;
+22		for(int i = 0;i<arr.length;i++) {
+23			int res = Math.min(ans,solve(target-arr[i],arr,dp));
+24			if(res!=99999999) {
+25				ans = Math.min(ans, res+1);
+26			}
+27			
 28		}
-29		boolean inc = solve(sum+arr[idx],idx+1,target,arr,dp);
-30		boolean exc = solve(sum,idx+1,target,arr,dp);
-31		return dp[idx][sum] = inc|| exc;
-32	}
-33}
+29		return dp[target] = ans;
+30		
+31	}
+32}
