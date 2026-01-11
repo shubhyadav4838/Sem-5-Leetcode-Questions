4b7eb2449b9ae8599a68e21551ed68a791149470
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-11 23:55:27 +0530

    Create 416-PartitionEqualSubsetSum

---
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -0,0 +1,34 @@
+// Last updated: 11/1/2026, 11:55:26 pm
+1class Solution {
+2    static Boolean[][] dp; 
+3	public static boolean canPartition(int[] arr) {
+4		int sum = 0;
+5		for(int x:arr) {
+6			sum+=x;
+7		}
+8		if(sum%2!=0) {
+9			return false;
+10		}
+11		int target = sum/2;
+12		dp = new Boolean[arr.length][sum+1];
+13		return solve(0,0,target,arr,dp);
+14		
+15	}
+16	
+17	private static boolean solve(int sum, int idx, int target,int[] arr, Boolean[][] dp ) {
+18		// TODO Auto-generated method stub
+19		if(sum==target) {
+20			return true;
+21		}
+22		if(sum>target || idx==arr.length) {
+23			return false;
+24		}
+25		
+26		if(dp[idx][sum]!=null) {
+27			return dp[idx][sum];
+28		}
+29		boolean inc = solve(sum+arr[idx],idx+1,target,arr,dp);
+30		boolean exc = solve(sum,idx+1,target,arr,dp);
+31		return dp[idx][sum] = inc|| exc;
+32	}
+33}
