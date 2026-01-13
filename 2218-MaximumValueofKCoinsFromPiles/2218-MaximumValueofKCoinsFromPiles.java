e0b9564a41d420883023f971fbe4ee1a9d0289d8
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-13 19:38:07 +0530

    File updated at 13/1/2026, 7:38:07 pm

--- 2218-MaximumValueofKCoinsFromPiles/2218-MaximumValueofKCoinsFromPiles.java
+++ 2218-MaximumValueofKCoinsFromPiles/2218-MaximumValueofKCoinsFromPiles.java
@@ -1,33 +1,24 @@
-// Last updated: 13/1/2026, 6:13:58 pm
+// Last updated: 13/1/2026, 7:38:07 pm
 1class Solution {
-2    static Integer[][] dp;
-3
-4    public static int maxValueOfCoins(List<List<Integer>> piles, int k) {
-5        int n = piles.size();
-6        dp = new Integer[n][k + 1];
-7        
-8        return solve(0, k, piles);
-9    }
-10
-11    private static int solve(int pileidx, int k, List<List<Integer>> piles) {
-12        if (k == 0) return 0;
-13        
-14        if (pileidx == piles.size()) return 0;
-15        if (dp[pileidx][k] != null) {
-16            return dp[pileidx][k];
-17        }
-18
-19        int maxVal = solve(pileidx + 1, k, piles);
-20        int currentPileSum = 0;        
-21        int limit = Math.min(k, piles.get(pileidx).size());
-22        
-23        for (int j = 0; j < limit; j++) {
-24       
-25            currentPileSum += piles.get(pileidx).get(j);
-26            int result = solve(pileidx + 1, k - (j + 1), piles);
-27            maxVal = Math.max(maxVal, currentPileSum + result);
-28        }
-29
-30        return dp[pileidx][k] = maxVal;
-31    }
-32}
+2    public int maxValueOfCoins(List<List<Integer>> piles, int k) {
+3        int n = piles.size();
+4        int[][] dp = new int[n + 1][k + 1];
+5
+6        for (int i = 1; i <= n; i++) {
+7            
+8            for (int w = 0; w <= k; w++) {
+9                
+10                dp[i][w] = dp[i - 1][w];
+11                int currentPileSum = 0;
+12                int limit = Math.min(w, piles.get(i - 1).size());
+13                
+14                for (int taken = 1; taken <= limit; taken++) {
+15                    currentPileSum += piles.get(i - 1).get(taken - 1);
+16                    dp[i][w] = Math.max(dp[i][w], dp[i - 1][w - taken] + currentPileSum);
+17                }
+18            }
+19        }
+20        
+21        return dp[n][k];
+22    }
+23}
