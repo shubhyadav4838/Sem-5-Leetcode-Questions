94c61341880c65f2fcd1c826bfecdc6db48ac2e9
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-13 13:26:51 +0530

    File updated at 13/1/2026, 1:26:49 pm

--- 518-CoinChangeII/518-CoinChangeII.java
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -1,13 +1,27 @@
-// Last updated: 13/1/2026, 1:02:17 pm
-1class Solution {
-2    public int change(int amount, int[] arr) {
-3        int[] dp = new int[amount+1];
-4		dp[0] = 1;
-5		for(int i = 0;i<arr.length;i++) {
-6			for(int j = arr[i];j<=amount;j++) {
-7				dp[j] += dp[j-arr[i]];
-8			}
-9		}
-10		return dp[amount];
-11    }
-12}
+// Last updated: 13/1/2026, 1:26:49 pm
+class Solution {
+
+    static {
+        for (int i = 0; i < 230; i++) {
+            change(2, new int[]{2});
+        }
+    }
+
+    public static int change(int amount, int[] coins) {
+        int totalCoins = coins.length;
+        int[] dp = new int[amount + 1];
+
+        // Base case: there’s 1 way to make amount 0 (no coins)
+        dp[0] = 1;
+
+        // For each coin denomination
+        for (int coinValue : coins) {
+            // For each amount from the coin value up to target amount
+            for (int currentAmount = coinValue; currentAmount <= amount; currentAmount++) {
+                dp[currentAmount] += dp[currentAmount - coinValue];
+            }
+        }
+
+        return dp[amount];
+    }
+}
