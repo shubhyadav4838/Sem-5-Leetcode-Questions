4d75cabefdce573e1fbb3065df4000fb48d3be32
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-17 15:18:13 +0530

    File updated at 17/1/2026, 3:18:12 pm

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,21 +1,20 @@
-// Last updated: 17/1/2026, 3:13:16 pm
+// Last updated: 17/1/2026, 3:18:12 pm
 1class Solution {
 2   public static int longestPalindromeSubseq(String s) {
-3		String reversed = new StringBuilder(s).reverse().toString();
-4		return longestCommonSubsequence(s,reversed);
-5	}
-6
-7	public static int longestCommonSubsequence(String text1, String text2) {
-8		int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-9		for (int i = 1; i <= text1.length(); i++) {
-10			for (int j = 1; j <= text2.length(); j++) {
-11				if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-12					dp[i][j] = 1 + dp[i - 1][j - 1];
-13				} else {
-14					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-15				}
-16			}
-17		}
-18		return dp[text1.length()][text2.length()];
-19	}
-20}
+3		int[][] dp = new int[s.length() + 1][s.length() + 1];
+4		int n = s.length();
+5		for (int i = 1; i <= n; i++) {
+6			for (int j = 1; j <= n; j++) {
+7				
+8				if (s.charAt(i - 1) == s.charAt(n-j)) {
+9					dp[i][j] = 1 + dp[i - 1][j - 1];
+10				} else {
+11					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+12				}
+13			}
+14		}
+15		return dp[n][n];
+16	}
+17
+18	
+19}
