7fc7e3a1c2dae20ae1329731b5267931d34cf903
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-19 01:15:26 +0530

    File updated at 19/1/2026, 1:15:24 am

--- 1312-MinimumInsertionStepstoMakeaStringPalindrome/1312-MinimumInsertionStepstoMakeaStringPalindrome.java
+++ 1312-MinimumInsertionStepstoMakeaStringPalindrome/1312-MinimumInsertionStepstoMakeaStringPalindrome.java
@@ -1,23 +1,35 @@
-// Last updated: 19/1/2026, 12:38:07 am
+// Last updated: 19/1/2026, 1:15:24 am
 1class Solution {
-2    public int minInsertions(String s) {
-3    
-4        int n = s.length();
-5        if(n==1){
-6            return 0;
-7        }
-8        int[][] dp = new int[n][n];
-9        for(int i = n-1;i>=0;i--){
-10            for(int j = i+1;j<n;j++){
-11               
-12                if(s.charAt(i)==s.charAt(j)){
-13                    dp[i][j] = dp[i+1][j-1];
-14                }
-15                else{
-16                    dp[i][j] = Math.min(dp[i+1][j],dp[i][j-1])+1;
-17                }
-18            }
-19        }
-20        return dp[0][n-1];
-21    }
-22}
+2    public int minDistance(String word1, String word2) {
+3        int m = word1.length();
+4        int n = word2.length();
+5        
+6        int[][] dp = new int[m + 1][n + 1];
+7
+8        for (int j = 0; j <= n; j++) {
+9            dp[0][j] = j;
+10        }
+11
+12        for (int i = 0; i <= m; i++) {
+13            dp[i][0] = i;
+14        }
+15
+16        for (int i = 1; i <= m; i++) {
+17            for (int j = 1; j <= n; j++) {
+18
+19                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+20                    dp[i][j] = dp[i - 1][j - 1];
+21                } else {
+22                    
+23                    int replace = dp[i - 1][j - 1]; 
+24                    int delete  = dp[i - 1][j];    
+25                    int insert  = dp[i][j - 1];     
+26                    
+27                    dp[i][j] = 1 + Math.min(replace, Math.min(delete, insert));
+28                }
+29            }
+30        }
+31        
+32        return dp[m][n];
+33    }
+34}
