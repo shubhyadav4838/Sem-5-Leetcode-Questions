8ad4bd357c200dcb5c772b00b3191d4f81207c9b
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-19 00:20:08 +0530

    Create 1312-MinimumInsertionStepstoMakeaStringPalindrome

---
+++ 1312-MinimumInsertionStepstoMakeaStringPalindrome/1312-MinimumInsertionStepstoMakeaStringPalindrome.java
@@ -0,0 +1,23 @@
+// Last updated: 19/1/2026, 12:20:06 am
+1class Solution {
+2    public int minInsertions(String s) {
+3    
+4        int n = s.length();
+5        if(n==1){
+6            return 0;
+7        }
+8        int[][] dp = new int[n][n];
+9        for(int i = n-1;i>=0;i--){
+10            for(int j = i+1;j<n;j++){
+11                dp[i][i] = 1;
+12                if(s.charAt(i)==s.charAt(j)){
+13                    dp[i][j] = dp[i+1][j-1]+2;
+14                }
+15                else{
+16                    dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);
+17                }
+18            }
+19        }
+20        return n-dp[0][n-1];
+21    }
+22}
