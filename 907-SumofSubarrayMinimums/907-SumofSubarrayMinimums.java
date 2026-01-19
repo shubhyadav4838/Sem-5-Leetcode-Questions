d3a6b714b0da40be20289171917da0ac7436f418
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-19 11:29:34 +0530

    File updated at 19/1/2026, 11:29:31 am

--- 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
+++ 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
@@ -1,51 +1,50 @@
-// Last updated: 19/1/2026, 11:27:59 am
+// Last updated: 19/1/2026, 11:29:31 am
 1class Solution {
-2    public int sumSubarrayMins(int[] arr) {
-3        return subMin(arr);
-4    }
-5   public static int subMin(int[] arr) {
-6        int n = arr.length;
-7        int[] pre = new int[n];
-8        int[] suff = new int[n];
-9        Stack<Integer> p = new Stack<>();
-10        Stack<Integer> s = new Stack<>();
-11        
-12        long MOD = (long)(1e9 + 7); 
-13
-14        for(int i = 0; i < n; i++) {
-15            while(!p.isEmpty() && arr[p.peek()] >= arr[i]) {
-16                p.pop();
-17            }
-18            if(p.isEmpty()) {
-19                pre[i] = -1;
-20            } else {
-21                pre[i] = p.peek();
-22            }
-23            p.push(i);
-24        }
-25        
-26        for(int i = n - 1; i >= 0; i--) {
-27            while(!s.isEmpty() && arr[s.peek()] > arr[i]) {
-28                s.pop();
-29            }
-30            if(s.isEmpty()) {
-31                suff[i] = n;
-32            } else {
-33                suff[i] = s.peek();
-34            }
-35            s.push(i);
-36        }
-37        
-38        long sum = 0; 
-39        for(int i = 0; i < n; i++) {
-40            long leftCount = i - pre[i];
-41            long rightCount = suff[i] - i;
-42            long totalSubarrays = (leftCount * rightCount) % MOD;  
-43            long contribution = (arr[i] * totalSubarrays) % MOD;
-44            
-45            sum = (sum + contribution) % MOD;
-46        }
-47        
-48        return (int) sum;
-49    }
-50}
+2    public static int sumSubarrayMins(int[] arr) {
+3		int n = arr.length;
+4		int[] pre =new int[n];
+5		int[] suf = new int[n];
+6		
+7		Stack<Integer> p = new Stack<>();
+8		Stack<Integer> s = new Stack<>();
+9		long MOD = (long)(1e9 + 7);
+10		
+11		for(int i = 0;i<n;i++) {
+12			while(!p.isEmpty() && arr[p.peek()] >= arr[i]) {
+13				p.pop();
+14			}
+15			
+16			if(p.isEmpty()) {
+17				pre[i] = -1;
+18			}else {
+19				pre[i] = p.peek();
+20			}
+21			p.push(i);
+22		}
+23		
+24		for(int i = n-1;i>=0;i--) {
+25			while(!s.isEmpty() && arr[s.peek()] > arr[i]) {
+26				s.pop();
+27			}
+28			
+29			if(s.isEmpty()) {
+30				suf[i] = n;
+31			}else {
+32				suf[i] = s.peek();
+33			}
+34			s.push(i);
+35		}
+36		
+37		long sum = 0;
+38		for(int i = 0;i<arr.length;i++) {
+39			long right = suf[i] - i;
+40			long left = i - pre[i];
+41			long cont = (right*left)%MOD;
+42			long val = (arr[i]*cont)%MOD;
+43			sum = (sum + val ) % MOD;
+44		}
+45		
+46		return (int)sum;
+47
+48	}
+49}
