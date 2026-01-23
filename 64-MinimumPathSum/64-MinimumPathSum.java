0b8112356ea2656d0adabbae089c78987f11c035
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-23 22:43:41 +0530

    File updated at 23/1/2026, 10:43:39 pm

--- 64-MinimumPathSum/64-MinimumPathSum.java
+++ 64-MinimumPathSum/64-MinimumPathSum.java
@@ -1,22 +1,23 @@
-// Last updated: 4/8/2025, 12:01:03 am
-class Solution {
-    public int minPathSum(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-
-        for (int col = 1; col < n; col++) {
-            grid[0][col] += grid[0][col - 1];
-        }
-
-        for (int row = 1; row < m; row++) {
-            grid[row][0] += grid[row - 1][0];
-        }
-
-        for (int row = 1; row < m; row++) {
-            for (int col = 1; col < n; col++) {
-                grid[row][col] += Math.min(grid[row - 1][col], grid[row][col - 1]);
-            }
-        }
-
-        return grid[m - 1][n - 1];
-    }
-}
+// Last updated: 23/1/2026, 10:43:39 pm
+1class Solution {
+2    public int minPathSum(int[][] grid) {
+3        int m = grid.length, n = grid[0].length;
+4
+5        for (int col = 1; col < n; col++) {
+6            grid[0][col] += grid[0][col - 1];
+7        }
+8
+9        for (int row = 1; row < m; row++) {
+10            grid[row][0] += grid[row - 1][0];
+11        }
+12
+13        for (int row = 1; row < m; row++) {
+14            for (int col = 1; col < n; col++) {
+15                grid[row][col] += Math.min(grid[row - 1][col], grid[row][col - 1]);
+16            }
+17        }
+18
+19        return grid[m - 1][n - 1];
+20    }
+21}
+22
