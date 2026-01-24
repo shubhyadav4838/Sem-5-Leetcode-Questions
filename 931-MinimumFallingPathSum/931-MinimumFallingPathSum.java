e85c00a54b5b4a8ace9c30763e1913b7621b0d26
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-24 17:18:45 +0530

    File updated at 24/1/2026, 5:18:44 pm

--- 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
+++ 931-MinimumFallingPathSum/931-MinimumFallingPathSum.java
@@ -1,33 +1,24 @@
-// Last updated: 10/9/2025, 12:21:13 pm
-class Solution {
-    public int minFallingPathSum(int[][] matrix) {
-        int ans = Integer.MAX_VALUE;
-		int[][] dp = new int[matrix.length][matrix[0].length];
-		for(int[] arr:dp) {
-			Arrays.fill(arr, Integer.MIN_VALUE);
-		}
-		for(int col=0;col<matrix[0].length;col++) {
-			ans = Math.min(ans, minFalling(matrix,0,col,dp));
-		}
-        return ans;
-        
-    }
-    public static int minFalling(int[][] matrix,int cr,int cc,int[][] dp) {
-		if(cc<0 || cc>=matrix[0].length) {
-			return Integer.MAX_VALUE;
-		}
-		if(cr==matrix.length -1) {
-			return matrix[cr][cc];
-		}
-		if(dp[cr][cc] != Integer.MIN_VALUE) {
-			return dp[cr][cc];
-		}
-		
-		int ld = minFalling(matrix,cr+1,cc-1,dp);
-		int rd = minFalling(matrix,cr+1,cc,dp);
-		int d = minFalling(matrix,cr+1,cc+1,dp);
-		return dp[cr][cc] = Math.min(ld,Math.min(rd, d)) + matrix[cr][cc];
-		
-		
-	}
-}
+// Last updated: 24/1/2026, 5:18:44 pm
+1class Solution {
+2    public static int minFallingPathSum(int[][] arr) {
+3		int n = arr.length;
+4		int m = arr[0].length;
+5		for(int i = 1;i<n;i++) {
+6			for(int j = 0;j<m;j++) {
+7				int right = 999999, left = 999999;
+8				if(j>0)left = arr[i-1][j-1];
+9				if(j<m-1)right = arr[i-1][j+1];
+10				arr[i][j] += Math.min(left, Math.min(arr[i-1][j], right));
+11				
+12			}
+13		}
+14		int ans = Integer.MAX_VALUE;
+15		for(int i = 0;i<m;i++) {
+16			ans = Math.min(ans, arr[n-1][i]);
+17		}
+18       
+19		return ans;
+20
+21	}
+22
+23}
