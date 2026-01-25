611d351e6c38d9c11de49fc62ac6ba9b3a17fb5b
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-25 15:04:36 +0530

    File updated at 25/1/2026, 3:04:36 pm

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,38 +1,35 @@
-// Last updated: 6/1/2026, 6:31:08 pm
+// Last updated: 25/1/2026, 3:04:36 pm
 1class Solution {
 2    public static int lengthOfLIS(int[] arr) {
 3		int n = arr.length;
-4		if(n==1)return 1;
-5		int[] dp = new int[n];
-6		
-7		
-8		dp[0] = arr[0];
-9		int len = 1;
-10		for(int i = 1;i<n;i++) {
-11			if(arr[i]<dp[len-1]) {
-12				int idx = binarySearch(arr[i],0,len-1,dp);
+4		int[] dp = new int[n+1];
+5		dp[0] = arr[0];
+6		int len = 1;
+7		for(int i = 1;i<n;i++) {
+8			if(dp[len-1]<arr[i]) {
+9				dp[len] = arr[i];
+10				len++;
+11			}else {
+12				int idx = search(dp,0,len-1,arr[i]);
 13				dp[idx] = arr[i];
-14			}else if(arr[i]>dp[len-1]) {
-15				dp[len] = arr[i];
-16				len++;
-17			}
-18		}
-19		return len;
-20		
-21		
-22	}
-23	
-24	public static int binarySearch(int item,int si,int ei,int[] arr) {
-25		int ans = 0;
-26		while(si<=ei) {
-27			int mid = (si+ei)/2;
-28			if(arr[mid]>=item) {
-29				ans = mid;
-30				ei = mid-1;
-31			}else {
-32				si = mid+1;
-33			}
-34		}
-35		return ans;
-36	}
-37}
+14			}
+15		}
+16		return len;
+17		
+18	}
+19
+20	private static int search(int[] dp, int lo, int hi, int target) {
+21		// TODO Auto-generated method stub
+22		int ans = 0;
+23		while(lo<=hi) {
+24			int mid = (lo+hi)/2;
+25			if(dp[mid]>=target) {
+26				ans = mid;
+27				hi = mid-1;
+28			}else {
+29				lo = mid+1;
+30			}
+31		}
+32		return ans;
+33	}
+34}
