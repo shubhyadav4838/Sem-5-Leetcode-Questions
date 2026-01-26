b7d0506a6ee400adb983bc08a9c0e1a882f58b24
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-26 15:32:36 +0530

    File updated at 26/1/2026, 3:32:35 pm

--- 312-BurstBalloons/312-BurstBalloons.java
+++ 312-BurstBalloons/312-BurstBalloons.java
@@ -1,31 +1,40 @@
-// Last updated: 18/9/2025, 12:31:08 pm
-class Solution {
-    public int maxCoins(int[] arr) {
-        int[] a = new int[arr.length+2];
-		a[0] = a[a.length-1]= 1;
-		for (int i = 0; i < arr.length; i++) {
-			a[i+1] = arr[i];
-		}
-		int[][] dp = new int[a.length][a.length];
-		return burstCoin(a,0,a.length-1,dp);
-        
-    }
-    public static int burstCoin(int[] a, int i, int j, int[][] dp) {
-		if(i+1 == j) {
-			return 0;
-		}
-		if(dp[i][j]!=0) {
-			return dp[i][j];
-		}
-		
-		int ans = Integer.MIN_VALUE;
-		for (int k = i+1; k < j; k++) {
-			int left = burstCoin(a,i,k,dp);
-			int right = burstCoin(a,k,j,dp);
-			int self = a[i] * a[j] * a[k];
-			ans = Math.max(ans, left+right+self);
-			
-		}
-		return dp[i][j] = ans;
-	}
-}
+// Last updated: 26/1/2026, 3:32:35 pm
+1class Solution {
+2    public static int maxCoins(int[] nums) {
+3		int n = nums.length;
+4		int[] arr = new int[n+2];
+5		arr[0] = arr[n+1] = 1;
+6		for(int i = 0;i<n;i++) {
+7			arr[i+1] = nums[i];
+8		}
+9		int[][] dp = new int[n+2][n+2];
+10		for(int[] x:dp) {
+11			Arrays.fill(x, -1);
+12		}
+13		
+14		return solve(arr,0,n+1,dp);
+15		
+16	
+17	}
+18	
+19	
+20	public static int solve(int[] arr,int i, int j, int[][]dp) {
+21		if(i==j) {	
+22			return arr[i];
+23		}
+24		
+25		if(dp[i][j]!=-1) {
+26			return dp[i][j];
+27		}
+28		
+29		int ans = 0;
+30		for(int k = i+1;k<j;k++) {
+31			int fs = solve(arr,i,k,dp);
+32			int ls = solve(arr,k,j,dp);
+33			int self = arr[i]*arr[k]*arr[j];
+34			ans = Math.max(ans, fs+ls+self);
+35			
+36		}
+37		return dp[i][j] = ans;
+38	}
+39}
