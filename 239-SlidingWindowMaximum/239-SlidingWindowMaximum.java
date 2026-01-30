a0d2a2765db2227746b6f471a71a19d84c255127
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-01-30 22:24:27 +0530

    File updated at 30/1/2026, 10:24:26 pm

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
@@ -1,28 +1,29 @@
-// Last updated: 3/8/2025, 11:59:18 pm
-class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        if (n == 0 || k == 0) return new int[0];
-
-        int[] result = new int[n - k + 1];
-        Deque<Integer> dq = new ArrayDeque<>();
-        
-        for (int i = 0; i < n; i++) {
-            if (!dq.isEmpty() && dq.peekFirst() == i - k) {
-                dq.pollFirst();
-            }
-
-            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
-                dq.pollLast();
-            }
-
-            dq.offerLast(i);
-
-            if (i >= k - 1) {
-                result[i - k + 1] = nums[dq.peekFirst()];
-            }
-        }
-
-        return result;
-    }
-}
+// Last updated: 30/1/2026, 10:24:26 pm
+1class Solution {
+2    public int[] maxSlidingWindow(int[] nums, int k) {
+3        int n = nums.length;
+4        if (n == 0 || k == 0) return new int[0];
+5
+6        int[] result = new int[n - k + 1];
+7        Deque<Integer> dq = new ArrayDeque<>();
+8        
+9        for (int i = 0; i < n; i++) {
+10            if (!dq.isEmpty() && dq.peekFirst() == i - k) {
+11                dq.pollFirst();
+12            }
+13
+14            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
+15                dq.pollLast();
+16            }
+17
+18            dq.offerLast(i);
+19
+20            if (i >= k - 1) {
+21                result[i - k + 1] = nums[dq.peekFirst()];
+22            }
+23        }
+24
+25        return result;
+26    }
+27}
+28
