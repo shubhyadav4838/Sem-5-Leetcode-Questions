ad0ef20a2c4d147439515f7c2577fef7a96c541e
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-02 19:51:17 +0530

    File updated at 2/2/2026, 7:51:16 pm

--- 2555-MaximizeWinFromTwoSegments/2555-MaximizeWinFromTwoSegments.java
+++ 2555-MaximizeWinFromTwoSegments/2555-MaximizeWinFromTwoSegments.java
@@ -1,22 +1,19 @@
-// Last updated: 2/2/2026, 7:50:16 pm
+// Last updated: 2/2/2026, 7:51:16 pm
 1class Solution {
 2    public static int maximizeWin(int[] arr, int k) {
-3		int n = arr.length;
-4		int[] dp = new int[n+1];
-5		int ans = 0;
-6		int si = 0;
-7		for(int ei = 0;ei<n;ei++) {
-8			while(si<n && arr[si]<arr[ei]-k) {
-9				si++;
-10			}
-11			int sec = ei-si+1;
-12			int first = si-1>=0? dp[si-1]:0;
-13			ans = Math.max(ans, first+sec);
-14			int pre = ei-1>=0 ?dp[ei-1]:0;
-15			dp[ei] = Math.max(pre,sec); 
-16		}
-17		
-18		return ans;
-19
-20	}
-21}
+3		int[] dp = new int[arr.length+1];
+4		int ans = 0;
+5		int left = 0;
+6		
+7		for(int i = 0;i<arr.length;i++) {
+8			while(arr[i]-arr[left]>k)left++;
+9			
+10			int range = i-left+1;
+11			
+12			dp[i+1] = Math.max(dp[i], range);
+13			ans = Math.max(ans, range+dp[left]);
+14		}
+15		return ans;
+16		
+17	}
+18}
