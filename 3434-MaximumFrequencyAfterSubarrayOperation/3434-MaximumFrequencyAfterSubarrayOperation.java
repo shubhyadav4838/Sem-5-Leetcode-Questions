308487a5eb45e2c78341af7832c23fd5a951325f
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-06 13:32:04 +0530

    Create 3434-MaximumFrequencyAfterSubarrayOperation

---
+++ 3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
@@ -0,0 +1,19 @@
+// Last updated: 6/2/2026, 1:32:03 pm
+1class Solution {
+2    public int maxFrequency(int[] nums, int k) {
+3        HashMap<Integer,Integer> map = new HashMap<>();
+4        int countk = 0;
+5        int max = 0;
+6        for(int x : nums){
+7            if(x==k){
+8                countk++;
+9            }else{
+10                int curr = map.getOrDefault(x,0)-countk;
+11                curr = Math.max(1,curr+1);
+12                map.put(x,curr+countk);
+13                max = Math.max(max,curr);
+14            }
+15        }
+16        return countk+max;
+17    }
+18}
