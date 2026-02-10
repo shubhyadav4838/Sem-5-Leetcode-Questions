4e48b98ddcc068a2079baeb6fe8bebae764158cb
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-10 12:52:25 +0530

    File updated at 10/2/2026, 12:52:24 pm

--- 3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
+++ 3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.java
@@ -1,4 +1,4 @@
-// Last updated: 10/2/2026, 12:00:46 pm
+// Last updated: 10/2/2026, 12:52:24 pm
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -15,34 +15,20 @@
 14 * }
 15 */
 16class Solution {
-17    long totalSum = 0;
-18    long maxProduct = 0;
-19
-20    public int maxProduct(TreeNode root) {
-21        totalSum = 0;
-22        maxProduct = 0;
-23        totalSum = getSum(root);
-24        
-25        getSumAndCheck(root);
-26        
-27        return (int)(maxProduct % 1_000_000_007);
-28    }
-29
-30    public long getSum(TreeNode root) {
-31        if (root == null) return 0;
-32        return root.val + getSum(root.left) + getSum(root.right);
-33    }
-34
-35    public long getSumAndCheck(TreeNode root) {
-36        if (root == null) return 0;
-37
-38        long leftSum = getSumAndCheck(root.left);
-39        long rightSum = getSumAndCheck(root.right);
-40        
-41        long currentSubtreeSum = leftSum + rightSum + root.val;
-42        long remainingSum = totalSum - currentSubtreeSum;
-43        
-44        maxProduct = Math.max(maxProduct, currentSubtreeSum * remainingSum);
-45        return currentSubtreeSum;
-46    }
-47}
+17    public TreeNode reverseOddLevels(TreeNode root) {
+18        dfs(root.left,root.right,0);
+19        return root;
+20    }
+21    public void dfs(TreeNode left, TreeNode right, int level){
+22        if(left==null || right==null)return;
+23
+24        if(level%2==0){
+25            int temp = left.val;
+26            left.val = right.val;
+27            right.val = temp;
+28        }
+29        dfs(left.left,right.right,level+1);
+30        dfs(left.right,right.left,level+1);
+31
+32    }
+33}
