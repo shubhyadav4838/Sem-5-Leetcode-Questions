84e007540470ba4109b65514835c2c72a907248e
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-13 00:32:43 +0530

    File updated at 13/2/2026, 12:32:43 am

--- 1373-MaximumSumBSTinBinaryTree/1373-MaximumSumBSTinBinaryTree.java
+++ 1373-MaximumSumBSTinBinaryTree/1373-MaximumSumBSTinBinaryTree.java
@@ -1,52 +1,47 @@
-// Last updated: 7/8/2025, 12:32:34 pm
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-		    public int maxSumBST(TreeNode root) {
-		    	return ValidBst(root).ans;
-		    	
-		        
-		    }
-		    class BstPair{
-				long min = Long.MAX_VALUE;
-				long max = Long.MIN_VALUE;
-				boolean isBst = true;
-				int sum = 0;
-				int ans = 0; //only maximum bst part ka sum jo bst h
-			}
-		  
-		    public BstPair ValidBst(TreeNode root) {
-		    	if(root == null) {
-		    		return new BstPair();
-		    	}
-		    	
-		    	BstPair lbp = ValidBst(root.left);
-		    	BstPair rbp = ValidBst(root.right);
-		    	BstPair sbp = new BstPair();
-		    	sbp.min = Math.min(lbp.min,Math.min(rbp.min, root.val) );
-		    	sbp.max = Math.max(lbp.max,Math.max(rbp.max, root.val) );
-		    	sbp.sum = lbp.sum+rbp.sum+root.val;
-		    	sbp.isBst = lbp.isBst && rbp.isBst && lbp.max<root.val && rbp.min>root.val;
-		    	if(sbp.isBst) {
-		    		sbp.ans = Math.max(lbp.ans,Math.max(rbp.ans, sbp.sum));
-		    	}else {
-		    		sbp.ans = Math.max(lbp.ans, rbp.ans);
-		    	}
-		    	return sbp;
-		    			
-		    	
-		    }
-		}
+// Last updated: 13/2/2026, 12:32:43 am
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    static class Pair{
+18        long min = Integer.MAX_VALUE;
+19        long max = Integer.MIN_VALUE;
+20        boolean isbst = true;;
+21        long ans = 0;
+22        long sum = 0;
+23
+24    }
+25    public static int maxSumBST(TreeNode root) {
+26        return (int)solve(root).ans;
+27    }
+28    public static Pair solve(TreeNode root){
+29        if(root==null)return new Pair();
+30
+31        Pair left = solve(root.left);
+32        Pair right = solve(root.right);
+33        Pair self = new Pair();
+34        self.sum = left.sum + right.sum + root.val;
+35        boolean isbst = root.val>left.max && root.val<right.min;
+36        self.isbst = isbst && left.isbst && right.isbst;
+37        self.max = Math.max(root.val,right.max);
+38        self.min = Math.min(root.val,left.min);
+39        self.ans = Math.max(left.ans,right.ans);
+40        if(self.isbst){
+41            self.ans = Math.max(self.ans,self.sum);
+42        }
+43
+44        return self;
+45    }
+46}
