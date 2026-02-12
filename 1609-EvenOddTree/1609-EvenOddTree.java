21878a29a9ddee32546d9f123ef95f9220113045
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-12 23:30:44 +0530

    File updated at 12/2/2026, 11:30:44 pm

--- 1609-EvenOddTree/1609-EvenOddTree.java
+++ 1609-EvenOddTree/1609-EvenOddTree.java
@@ -1,88 +1,57 @@
-// Last updated: 27/8/2025, 11:16:22 pm
-/**
-
- * Definition for a binary tree node.
-
- * public class TreeNode {
-
- *     int val;
-
- *     TreeNode left;
-
- *     TreeNode right;
-
- *     TreeNode() {}
-
- *     TreeNode(int val) { this.val = val; }
-
- *     TreeNode(int val, TreeNode left, TreeNode right) {
-
- *         this.val = val;
-
- *         this.left = left;
-
- *         this.right = right;
-
- *     }
-
- * }
-
- */
-
-class Solution {
-
-    public boolean isEvenOddTree(TreeNode root) {
-
-        if(root == null) return true;
-
-
-        Queue<TreeNode> q = new LinkedList<>();
-
-        q.offer(root);
-
-        int level = 0;
-
-
-        while(!q.isEmpty()){
-
-            int size = q.size();
-
-            int prev = (level % 2 == 0) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-
-
-            for(int i = 0; i<size; i++){
-
-                TreeNode nn = q.poll();
-
-                int curr = nn.val;
-
-
-            if(level % 2 == 0){
-
-                if(curr % 2 == 0 || curr <= prev) return false;
-
-            }
-
-            else{
-
-                if(curr % 2 != 0 || curr >= prev) return false;
-
-            }
-
-            prev = curr;
-
-            if(nn.left != null) q.offer(nn.left);
-
-            if(nn.right != null) q.offer(nn.right);
-
-        }
-
-        level++;
-
-        }
-
-        return true;
-
-    }
-
-}
+// Last updated: 12/2/2026, 11:30:44 pm
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isEvenOddTree(TreeNode root) {
+18        if(root==null){
+19           
+20            return true;
+21        }
+22        Queue<TreeNode> q = new LinkedList<>();
+23        q.add(root);
+24        int level = 0;
+25        while(!q.isEmpty()){
+26            int size = q.size();
+27            int pre = level%2==0 ? Integer.MIN_VALUE:Integer.MAX_VALUE;
+28
+29            for(int i = 0;i<size && !q.isEmpty();i++){
+30                TreeNode curr = q.poll();
+31                int x = curr.val;
+32                if(level%2==0 ){
+33                    if(x%2==0 || x<=pre){
+34                        
+35                        return false;
+36                    }    
+37                }else{
+38                    if(x%2!=0 || x>=pre){
+39                       
+40                        return false;
+41                    }
+42                }
+43                pre = x;
+44                if(curr.left!=null){
+45                    q.add(curr.left);
+46                }
+47                if(curr.right!=null){
+48                    q.add(curr.right);
+49                }
+50
+51            }
+52            level++;
+53        }
+54        return true;
+55    }
+56}
