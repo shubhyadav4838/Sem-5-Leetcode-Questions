b13c8c34884d8ede5c3e163d2763ea77b100137a
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-13 16:58:14 +0530

    File updated at 13/2/2026, 4:58:13 pm

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,14 +1,19 @@
-// Last updated: 4/8/2025, 12:01:05 am
-class Solution {
-    public int uniquePaths(int m, int n) {
-        int [] arr=new int[n];
-        arr[0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=1;j<n;j++){
-                arr[j]+=arr[j-1];
-            }
-        }
-        return arr[n-1];
-
-    }
-}
+// Last updated: 13/2/2026, 4:58:13 pm
+1class Solution {
+2    public int uniquePaths(int m, int n) {
+3        
+4        int[][] dp =new int[m][n];
+5        dp[0][0] = 1;
+6        for(int i = 0;i<m;i++){
+7            for(int j = 0;j<n;j++){
+8                if(j+1<n){
+9                    dp[i][j+1] += dp[i][j];
+10                }
+11                if(i+1<m){
+12                    dp[i+1][j] += dp[i][j];
+13                }
+14            }
+15        }
+16        return dp[m-1][n-1];
+17    }
+18}
