d6d7d11e4f602e8e63ce4a975227f97b4fbd6109
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-16 19:11:41 +0530

    File updated at 16/2/2026, 7:11:40 pm

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -1,20 +1,22 @@
-// Last updated: 12/1/2026, 1:30:26 am
+// Last updated: 16/2/2026, 7:11:40 pm
 1class Solution {
-2    public int coinChange(int[] coins, int amount) {
-3        int[] dp = new int[amount + 1];
-4        
-5        Arrays.fill(dp, amount + 1);
-6        
-7        dp[0] = 0;
-8        
-9        for (int i = 1; i <= amount; i++) {
-10            for (int coin : coins) {
-11                if (coin <= i) {
-12                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
-13                }
-14            }
-15        }
-16        
-17        return dp[amount] > amount ? -1 : dp[amount];
-18    }
-19}
+2    public boolean canPartition(int[] arr) {
+3        int sum = 0;
+4        for(int x:arr){
+5            sum+=x;
+6        }
+7        if(sum%2!=0)return false;
+8        int target = sum/2;
+9        boolean[] dp = new boolean[target+1];
+10        dp[0] = true;
+11        for(int i = 0;i<arr.length;i++){
+12            for(int j = target; j>=arr[i] ;j--){
+13                if(dp[j-arr[i]]==true){
+14                    dp[j] = true;
+15                }
+16            }
+17        }
+18        return dp[target];
+19        
+20    }
+21}
