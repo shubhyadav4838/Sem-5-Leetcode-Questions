945ecbadd8fe696c4ff9c0657a7f4ded9f436f4e
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-16 19:24:13 +0530

    File updated at 16/2/2026, 7:24:12 pm

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -1,37 +1,22 @@
-// Last updated: 16/2/2026, 7:21:28 pm
+// Last updated: 16/2/2026, 7:24:12 pm
 1class Solution {
-2   public static boolean canPartition(int[] arr) {
-3		int sum = 0;
+2    public boolean canPartition(int[] arr) {
+3        int sum = 0;
 4        for(int x:arr){
 5            sum+=x;
 6        }
 7        if(sum%2!=0)return false;
 8        int target = sum/2;
-9        Boolean[][] dp = new Boolean[arr.length+1][target+1];
-10        return solve(arr,0,target,dp);
-11	}
-12	
-13	public static boolean solve(int[] arr, int idx, int target, Boolean[][] dp) {
-14		if(target==0) {
-15			return true;
-16		}
-17		if(target<0) {
-18			return false;
-19		}
-20		if(idx==arr.length)return false;
-21		
-22		if(dp[idx][target]!=null) {
-23			return dp[idx][target];
-24		}
-25		boolean inc = false;
-26		if(target-arr[idx]>=0) {
-27			inc = solve(arr,idx+1,target-arr[idx],dp);
-28			
-29		}
-30		boolean exc = solve(arr,idx+1,target,dp);
-31		
-32		return dp[idx][target] = inc || exc;
-33		
-34		
-35	}
-36}
+9        boolean[] dp = new boolean[target+1];
+10        dp[0] = true;
+11        for(int i = 0;i<arr.length;i++){
+12            for(int j = target; j>=arr[i] ;j--){
+13                if(dp[j-arr[i]]==true){
+14                    dp[j] = true;
+15                }
+16            }
+17        }
+18        return dp[target];
+19        
+20    }
+21}
