bd37203fda6f8b87d1e596550f714061b9da518d
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-16 20:01:44 +0530

    File updated at 16/2/2026, 8:01:42 pm

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,18 +1,17 @@
-// Last updated: 13/1/2026, 12:13:08 pm
+// Last updated: 16/2/2026, 8:01:42 pm
 1class Solution {
-2    public static int coinChange(int[] arr, int amount) {
+2    public static int coinChange(int[] coins, int amount) {
 3		int[] dp = new int[amount+1];
 4		Arrays.fill(dp, amount+1);
-5		dp[0] = 0;
-6		
-7		for(int i = 1;i<=amount ;i++) {
-8			for(int coin:arr) {
+5		
+6		dp[0] = 0;
+7		for(int i = 1;i<dp.length;i++) {
+8			for(int coin : coins) {
 9				if(coin<=i) {
-10					dp[i] = Math.min(dp[i], dp[i-coin]+1);
+10					dp[i] = Math.min(dp[i],dp[i-coin]+1);
 11				}
 12			}
 13		}
-14		return dp[amount] > amount ?-1:dp[amount];
-15		
-16	}
-17}
+14		return dp[amount] >amount ? -1 : dp[amount];
+15	}
+16}
