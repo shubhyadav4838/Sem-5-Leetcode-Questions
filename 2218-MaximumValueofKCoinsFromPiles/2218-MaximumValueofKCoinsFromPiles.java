812c33dae85fba637487c219cbb234058e09eb33
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-17 19:51:06 +0530

    File updated at 17/2/2026, 7:51:06 pm

--- 2218-MaximumValueofKCoinsFromPiles/2218-MaximumValueofKCoinsFromPiles.java
+++ 2218-MaximumValueofKCoinsFromPiles/2218-MaximumValueofKCoinsFromPiles.java
@@ -1,24 +1,31 @@
-// Last updated: 13/1/2026, 7:38:07 pm
+// Last updated: 17/2/2026, 7:51:06 pm
 1class Solution {
 2    public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-3        int n = piles.size();
-4        int[][] dp = new int[n + 1][k + 1];
-5
-6        for (int i = 1; i <= n; i++) {
-7            
-8            for (int w = 0; w <= k; w++) {
-9                
-10                dp[i][w] = dp[i - 1][w];
-11                int currentPileSum = 0;
-12                int limit = Math.min(w, piles.get(i - 1).size());
-13                
-14                for (int taken = 1; taken <= limit; taken++) {
-15                    currentPileSum += piles.get(i - 1).get(taken - 1);
-16                    dp[i][w] = Math.max(dp[i][w], dp[i - 1][w - taken] + currentPileSum);
-17                }
-18            }
-19        }
-20        
-21        return dp[n][k];
-22    }
-23}
+3        Long[][] dp = new Long[piles.size() + 1][k + 1];
+4        return (int) (solve(piles, k, 0, dp));
+5    }
+6
+7    public static long solve(List<List<Integer>> piles, int k, int p, Long[][] dp) {
+8        if (k <= 0) {
+9            return 0;
+10        }
+11        if (p >= piles.size()) {
+12            return 0;
+13        }
+14
+15        if (dp[p][k] != null) {
+16            return dp[p][k];
+17        }
+18        long maxval = solve(piles,k,p+1,dp);
+19        int limit = Math.min(k,piles.get(p).size());
+20        long curr = 0;
+21        for(int i = 0; i< limit; i++){
+22            curr += piles.get(p).get(i);
+23            long res = solve(piles,k-(i+1),p+1,dp);
+24            maxval = Math.max(maxval,curr+res);
+25        }
+26        
+27
+28        return dp[p][k] = maxval;
+29    }
+30}
