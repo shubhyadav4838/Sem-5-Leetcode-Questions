b5d8345b142ceee81062c0a68d962ecd7e8e0cac
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-17 18:58:20 +0530

    File updated at 17/2/2026, 6:58:19 pm

--- 518-CoinChangeII/518-CoinChangeII.java
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -1,27 +1,24 @@
-// Last updated: 13/1/2026, 1:26:49 pm
-class Solution {
-
-    static {
-        for (int i = 0; i < 230; i++) {
-            change(2, new int[]{2});
-        }
-    }
-
-    public static int change(int amount, int[] coins) {
-        int totalCoins = coins.length;
-        int[] dp = new int[amount + 1];
-
-        // Base case: there’s 1 way to make amount 0 (no coins)
-        dp[0] = 1;
-
-        // For each coin denomination
-        for (int coinValue : coins) {
-            // For each amount from the coin value up to target amount
-            for (int currentAmount = coinValue; currentAmount <= amount; currentAmount++) {
-                dp[currentAmount] += dp[currentAmount - coinValue];
-            }
-        }
-
-        return dp[amount];
-    }
-}
+// Last updated: 17/2/2026, 6:58:19 pm
+1class Solution {
+2    public static int change(int amount, int[] arr) {
+3		int[][] dp = new int[arr.length+1][amount+1];
+4		
+5		for(int i = 0;i<dp.length;i++) {
+6			dp[i][0] = 1;
+7		}
+8		
+9		for(int i = 1;i<dp.length;i++) {
+10			for(int am = 1;am<dp[0].length;am++ ) {
+11				int exc = dp[i-1][am];
+12				int inc = 0;
+13				if(am>=arr[i-1]) {
+14					inc = dp[i][am-arr[i-1]];
+15				}
+16				dp[i][am] = exc+inc;
+17			}
+18		}
+19		return dp[arr.length][amount];
+20
+21	}
+22
+23}
