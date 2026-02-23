d05fe36f8325a3e1d159afa204df5e8aa22140ea
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-23 15:51:44 +0530

    File updated at 23/2/2026, 3:51:44 pm

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,13 +1,15 @@
-// Last updated: 3/8/2025, 11:59:44 pm
-class Solution {
-    public int rob(int[] nums) {
-        int prev = 0;
-        int ans = 0;
-        for (int i : nums) {
-            int temp = Math.max(ans, prev + i);
-            prev = ans;
-            ans = temp;
-        }
-        return ans;        
-    }
-}
+// Last updated: 23/2/2026, 3:51:44 pm
+1class Solution {
+2    public int rob(int[] nums) {
+3        int ans = 0;
+4        int pre = 0;
+5        for(int x:nums){
+6            int temp = Math.max(ans,pre+x);
+7            pre = ans;
+8            ans = temp;
+9        }
+10        return ans;
+11
+12        
+13    }
+14}
