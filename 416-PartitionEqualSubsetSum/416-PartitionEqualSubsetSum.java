56e0699ee5185e1f1d03c670efa8933beee3dccd
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-02-23 19:02:20 +0530

    File updated at 23/2/2026, 7:02:19 pm

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -1,22 +1,27 @@
-// Last updated: 23/2/2026, 4:41:06 pm
+// Last updated: 23/2/2026, 7:02:19 pm
 1class Solution {
 2    public boolean canPartition(int[] arr) {
 3        int sum = 0;
-4        for(int x:arr){
-5            sum+=x;
+4        for (int x : arr) {
+5            sum += x;
 6        }
-7        if(sum%2!=0)return false;
-8        int target = sum/2;
-9        boolean[] dp = new boolean[target+1];
-10        dp[0] = true;
-11        for(int i = 0;i<arr.length;i++){
-12            for(int j = target; j>=arr[i] ;j--){
-13                if(dp[j-arr[i]]==true){
-14                    dp[j] = true;
-15                }
-16            }
-17        }
-18        return dp[target];
-19        
-20    }
-21}
+7        if (sum % 2 != 0) {
+8            return false;
+9        }
+10        sum = sum / 2;
+11        boolean[] dp = new boolean[sum + 1];
+12        dp[0] = true;
+13        System.out.println(sum);
+14        for (int i = 0; i < arr.length; i++) {
+15            for (int am = sum; am >= arr[i]; am--) {
+16                if (dp[am - arr[i]] == true) {
+17                    dp[am] = true;
+18                }
+19            }
+20           
+21        }
+22
+23        return dp[dp.length - 1];
+24
+25    }
+26}
