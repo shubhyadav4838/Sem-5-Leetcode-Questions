9e9899d43807cc38103898ccad7a57e24e3170f5
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-03-15 23:27:17 +0530

    File updated at 15/3/2026, 11:27:16 pm

--- 743-NetworkDelayTime/743-NetworkDelayTime.java
+++ 743-NetworkDelayTime/743-NetworkDelayTime.java
@@ -1,24 +1,31 @@
-// Last updated: 15/3/2026, 10:48:15 pm
+// Last updated: 15/3/2026, 11:27:16 pm
 1class Solution {
-2    public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-3        int n = piles.size();
-4        int[][] dp = new int[n + 1][k + 1];
-5
-6        for (int i = 1; i <= n; i++) {
-7            
-8            for (int w = 0; w <= k; w++) {
-9                
-10                dp[i][w] = dp[i - 1][w];
-11                int currentPileSum = 0;
-12                int limit = Math.min(w, piles.get(i - 1).size());
-13                
-14                for (int taken = 1; taken <= limit; taken++) {
-15                    currentPileSum += piles.get(i - 1).get(taken - 1);
-16                    dp[i][w] = Math.max(dp[i][w], dp[i - 1][w - taken] + currentPileSum);
-17                }
-18            }
-19        }
-20        
-21        return dp[n][k];
-22    }
-23}
+2    public static int maxValueOfCoins(List<List<Integer>> piles, int k) {
+3		Integer[][] dp = new Integer[piles.size()+1][k+1];
+4		int ans = solve(piles, k,dp, 0 );
+5		return ans;
+6    }
+7
+8	private static int solve(List<List<Integer>> piles, int k, Integer[][] dp, int idx) {
+9		// TODO Auto-generated method stub
+10		if(k==0)return 0;
+11		if(idx>=piles.size()) {
+12			return -9999999;
+13		}
+14		if(dp[idx][k]!=null) {
+15			return dp[idx][k];
+16		}
+17		int ans = solve(piles,k,dp,idx+1);
+18		int curr = 0;
+19		int limit = Math.min(k, piles.get(idx).size());
+20		for(int i = 0; i<limit;i++) {
+21			curr += piles.get(idx).get(i);
+22			int req = solve(piles,k-i-1,dp,idx+1);
+23
+24				ans = Math.max(ans, req+curr);
+25
+26		}
+27		return dp[idx][k]=ans;
+28		
+29	}
+30}
