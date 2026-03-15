5ba5b9d989c27a9cddaca4111e0fc02a009b0ce3
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-03-15 22:14:34 +0530

    File updated at 15/3/2026, 10:14:34 pm

--- 547-NumberofProvinces/547-NumberofProvinces.java
+++ 547-NumberofProvinces/547-NumberofProvinces.java
@@ -1,54 +1,35 @@
-// Last updated: 10/1/2026, 1:50:35 pm
+// Last updated: 15/3/2026, 10:14:34 pm
 1class Solution {
-2    public static HashMap<Integer, ArrayList<Integer>> map;
-3
-4	public static void add(int e1, int e2) {
-5		if (e1 == e2)
-6			return;
-7		map.get(e1).add(e2);
-8
-9	}
-10
-11	public static int[] findOrder(int num, int[][] arr) {
-12		map = new HashMap<>();
-13		for (int i = 0; i < num; i++) {
-14			map.put(i, new ArrayList<>());
-15		}
-16
-17		int[] indegree = new int[num];
-18		Queue<Integer> q = new LinkedList<>();
-19		int count = 0;
-20		int[] ans = new int[num];
-21		int idx = 0;
-22		
-23		for (int i = 0; i < arr.length; i++) {
-24				add(arr[i][1], arr[i][0]);
-25				indegree[arr[i][0]]++;
-26			
-27		}
-28		for(int i = 0;i<indegree.length;i++) {
-29			if(indegree[i]==0) {
-30				q.add(i);
-31			}
-32		}
-33		
-34		while(!q.isEmpty()) {
-35			int r = q.poll();
-36			ans[idx++] = r;
-37			count++;
-38			
-39			for(int nbrs: map.get(r)) {
-40				indegree[nbrs]--;
-41				if(indegree[nbrs]==0) {
-42					q.add(nbrs);
-43				}
-44			}
-45		}
-46		if(num!=count) {
-47			return new int[0];
-48		}
-49		return ans;
-50		
-51		
-52	}
-53}
+2    public int findCircleNum(int[][] arr) {
+3        int n = arr.length;
+4        boolean[] visited = new boolean[n]; 
+5        int compo = 0;
+6
+7        
+8        for (int i = 0; i < n; i++) {
+9           
+10            if (!visited[i]) {
+11                compo++;
+12                bfs(i, arr, visited);
+13            }
+14        }
+15        return compo;
+16    }
+17
+18    private void bfs(int startNode, int[][] arr, boolean[] visited) {
+19        Queue<Integer> q = new LinkedList<>();
+20        q.add(startNode);
+21        visited[startNode] = true;
+22
+23        while (!q.isEmpty()) {
+24            int curr = q.poll();
+25
+26            for (int neighbor = 0; neighbor < arr.length; neighbor++) {
+27                if (arr[curr][neighbor] == 1 && !visited[neighbor]) {
+28                    visited[neighbor] = true; 
+29                    q.add(neighbor);
+30                }
+31            }
+32        }
+33    }
+34}
