f70bdd9310b7df3e4dd7f53b91c768ae8fb497fa
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-03-18 23:02:36 +0530

    File updated at 18/3/2026, 11:02:34 pm

--- 1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray/1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java
+++ 1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray/1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java
@@ -1,4 +1,4 @@
-// Last updated: 18/3/2026, 11:02:22 pm
+// Last updated: 18/3/2026, 11:02:34 pm
 1class Solution {
 2    public int minNumberOperations(int[] target) {
 3        if(target.length==1){
@@ -10,11 +10,9 @@
 9                sum += (target[i]-target[i-1]);
 10            }
 11        }
-12        // if(target[0]>target[1]){
-13        //     sum += (target[0]-target[1])+1;
-14        // }else{
-15            sum += target[0];
-16        // }
-17        return sum;
-18    }
-19}
+12        
+13            sum += target[0];
+14      
+15        return sum;
+16    }
+17}
