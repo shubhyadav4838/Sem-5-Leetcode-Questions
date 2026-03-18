32f96886b7ebc770770e837c37030e14cf98c3b1
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-03-18 23:39:01 +0530

    File updated at 18/3/2026, 11:38:59 pm

--- 1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray/1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java
+++ 1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray/1526-MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java
@@ -1,18 +1,22 @@
-// Last updated: 18/3/2026, 11:02:34 pm
+// Last updated: 18/3/2026, 11:38:59 pm
 1class Solution {
-2    public int minNumberOperations(int[] target) {
-3        if(target.length==1){
-4            return target[0];
-5        }
-6        int sum = 0;
-7        for(int i = 1;i<target.length; i++){
-8            if(target[i]>target[i-1]){
-9                sum += (target[i]-target[i-1]);
-10            }
-11        }
-12        
-13            sum += target[0];
-14      
-15        return sum;
-16    }
-17}
+2    public int totalSteps(int[] arr) {
+3        int n = arr.length;
+4        int[] dp = new int[n];
+5        int ans = 0;
+6        Stack<Integer> st = new Stack<>();
+7        for(int i = 0; i<n;i++){
+8            int curr = 0;
+9            while(!st.isEmpty() && arr[st.peek()]<=arr[i]){
+10                curr = Math.max(curr,dp[st.pop()]);
+11            }
+12
+13            if(!st.isEmpty()){
+14                dp[i] = curr+1;
+15                ans = Math.max(dp[i],ans);
+16            }
+17            st.push(i);
+18        }
+19        return ans;
+20    }
+21}
