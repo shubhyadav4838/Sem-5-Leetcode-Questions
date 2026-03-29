f05402ed8d1eb151f2961905629ec75c81dd2a1a
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-03-29 13:59:51 +0530

    File updated at 29/3/2026, 1:59:51 pm

--- 376-WiggleSubsequence/376-WiggleSubsequence.java
+++ 376-WiggleSubsequence/376-WiggleSubsequence.java
@@ -1,19 +1,35 @@
-// Last updated: 29/3/2026, 1:51:43 pm
+// Last updated: 29/3/2026, 1:59:51 pm
 1class Solution {
 2    public int wiggleMaxLength(int[] arr) {
-3         if(arr.length==1){
-4            return 1;
-5        }
-6        int up = 1;
-7        int down = 1;
-8        for(int i=1;i<arr.length;i++){
-9            if(arr[i]>arr[i-1]){
-10                up = down+1;
-11            }else if(arr[i]<arr[i-1]){
-12                down = up+1;
-13            }
-14        }
-15        return Math.max(up,down);
-16        
-17    }
-18}
+3        if (arr.length <= 1) return arr.length;
+4        
+5        Integer[][] dp = new Integer[arr.length][2];
+6        
+7        int startWithUp = 1 + solve(1, 1, arr, dp);
+8        int startWithDown = 1 + solve(1, 0, arr, dp);
+9        
+10        return Math.max(startWithUp, startWithDown);
+11    }
+12
+13    private int solve(int i, int expectingUp, int[] arr, Integer[][] dp) {
+14        if (i == arr.length) {
+15            return 0; 
+16        }
+17        
+18        if (dp[i][expectingUp] != null) {
+19            return dp[i][expectingUp];
+20        }
+21        
+22        int skip = solve(i + 1, expectingUp, arr, dp);
+23        
+24        int take = 0;
+25        if (expectingUp == 1 && arr[i] > arr[i - 1]) {
+26            take = 1 + solve(i + 1, 0, arr, dp);
+27        } 
+28        else if (expectingUp == 0 && arr[i] < arr[i - 1]) {
+29            take = 1 + solve(i + 1, 1, arr, dp);
+30        }
+31        
+32        return dp[i][expectingUp] = Math.max(skip, take);
+33    }
+34}
