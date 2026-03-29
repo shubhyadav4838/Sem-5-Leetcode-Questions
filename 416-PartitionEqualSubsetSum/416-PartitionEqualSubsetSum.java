ba39b67ec60d3f0525126b83f82028a4656a6ed4
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-03-29 12:21:42 +0530

    File updated at 29/3/2026, 12:21:42 pm

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -1,27 +1,33 @@
-// Last updated: 23/2/2026, 7:02:19 pm
+// Last updated: 29/3/2026, 12:21:42 pm
 1class Solution {
-2    public boolean canPartition(int[] arr) {
-3        int sum = 0;
-4        for (int x : arr) {
-5            sum += x;
-6        }
-7        if (sum % 2 != 0) {
-8            return false;
-9        }
-10        sum = sum / 2;
-11        boolean[] dp = new boolean[sum + 1];
-12        dp[0] = true;
-13        System.out.println(sum);
-14        for (int i = 0; i < arr.length; i++) {
-15            for (int am = sum; am >= arr[i]; am--) {
-16                if (dp[am - arr[i]] == true) {
-17                    dp[am] = true;
-18                }
-19            }
-20           
-21        }
-22
-23        return dp[dp.length - 1];
-24
-25    }
-26}
+2    public static boolean canPartition(int[] arr) {
+3		int sum = 0;
+4		for(int x:arr) {
+5			sum += x;
+6		}
+7		if(sum%2!=0) {
+8			return false;
+9		}
+10		
+11		sum = sum/2;
+12		Boolean[][] dp = new Boolean[arr.length][sum+1];
+13		return solve(0,arr,sum, dp);
+14	}
+15	
+16	public static boolean solve(int idx,int[] arr, int sum, Boolean[][] dp) {
+17		if(sum==0)return true;
+18		if(sum<0 || idx>=arr.length) return false;
+19		
+20		if(dp[idx][sum]!=null) {
+21			return dp[idx][sum];
+22		}
+23		
+24		boolean exc = solve(idx+1, arr,sum, dp);
+25		boolean inc = false;
+26		if(arr[idx]<=sum) {
+27			inc = solve(idx+1,arr,sum-arr[idx], dp);
+28		}
+29		
+30		return dp[idx][sum] = exc | inc;
+31	}
+32}
