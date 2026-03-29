e2820bb7c9134b5f3434d48dced9216b0f64582e
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-03-29 13:51:43 +0530

    File updated at 29/3/2026, 1:51:43 pm

--- 376-WiggleSubsequence/376-WiggleSubsequence.java
+++ 376-WiggleSubsequence/376-WiggleSubsequence.java
@@ -1,41 +1,19 @@
-// Last updated: 29/3/2026, 1:49:30 pm
+// Last updated: 29/3/2026, 1:51:43 pm
 1class Solution {
-2    
-3public static int wiggleMaxLength(int[] arr) {
-4        if(arr.length==1)return 1;
-5    	int[] dp = new int[arr.length];
-6    	Arrays.fill(dp,-1);
-7        int i=0;
-8        while(i<arr.length-1 && arr[i]==arr[i+1])i++;
-9    	
-10        return wiggle(0,i,1,arr,dp);
-11        
-12    }
-13
-14	private static int wiggle(int diff, int i, int len, int[] arr, int[] dp) {
-15		// TODO Auto-generated method stub
-16		while(i<arr.length-1 && arr[i]==arr[i+1])i++;
-17		if(i>=arr.length-1) {
-18			return len;
-19		}
-20		
-21		if(dp[i] != -1) {
-22			return dp[i];
-23		}
-24		
-25		int a = 0;
-26		int b = 0;
-27		
-28		if(diff<=0 &&  arr[i]<arr[i+1]) {
-29			a = wiggle(arr[i+1]-arr[i], i+1, len+1,arr,dp);
-30		}
-31		if(diff>=0 && arr[i]>arr[i+1]) {
-32			int c = wiggle(arr[i+1]-arr[i], i+1,len+1,arr,dp);
-33			a = Math.max(a, c);
-34			
-35		}
-36		b = wiggle(diff,i+1, len, arr,dp);
-37		return dp[i] = Math.max(a, b);
-38		
-39	}
-40}
+2    public int wiggleMaxLength(int[] arr) {
+3         if(arr.length==1){
+4            return 1;
+5        }
+6        int up = 1;
+7        int down = 1;
+8        for(int i=1;i<arr.length;i++){
+9            if(arr[i]>arr[i-1]){
+10                up = down+1;
+11            }else if(arr[i]<arr[i-1]){
+12                down = up+1;
+13            }
+14        }
+15        return Math.max(up,down);
+16        
+17    }
+18}
