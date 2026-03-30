a8f822c51d840820503dccca100e64b8fda93905
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-03-30 15:48:53 +0530

    File updated at 30/3/2026, 3:48:53 pm

--- 1186-MaximumSubarraySumwithOneDeletion/1186-MaximumSubarraySumwithOneDeletion.java
+++ 1186-MaximumSubarraySumwithOneDeletion/1186-MaximumSubarraySumwithOneDeletion.java
@@ -1,29 +1,34 @@
-// Last updated: 30/3/2026, 3:48:01 pm
-1class Solution {
-2   public static int maximumSum(int[] arr) {
-3		int n = arr.length;
-4		
-5		int ans = Integer.MIN_VALUE;
-6		int[][]dp = new int[n+1][2];
-7		for(int[] x:dp) {
-8			Arrays.fill(x, -999999);
-9		}
-10		
-11	
-12		for(int i = n-1;i>=0;i--) {
-13			for(int delete = 0; delete<2; delete++) {
-14				
-15				int old = arr[i] + dp[i+1][delete];
-16				int ifdel = -999999;
-17				if(arr[i]<0 && delete==0) {
-18					ifdel = dp[i+1][1];
-19				}
-20				dp[i][delete] = Math.max(arr[i], Math.max(old, ifdel));
-21				ans = Math.max(ans, dp[i][delete]);
-22			}
-23		}
-24		return ans;
-25		
-26
-27	}
-28}
+// Last updated: 30/3/2026, 3:48:53 pm
+class Solution {
+    public int maximumSum(int[] arr) {
+        // two options 
+        // no deletion  max in no deletion plus curr and curr 
+        // one delerion max in add curr to one deletion or delete curr i.e no deletion from prev index
+
+        int nodeletion,onedeletion,ans;
+        nodeletion=arr[0];
+        onedeletion=Integer.MIN_VALUE;
+        ans=arr[0];
+        for(int i=1;i<arr.length;i++){
+            int curr=arr[i];
+            int prevnodeletion=nodeletion;
+            int prevonedeletion=onedeletion;
+
+            nodeletion=Math.max(curr,prevnodeletion+curr);
+            int v2;
+            if(prevonedeletion==Integer.MIN_VALUE){
+                v2=arr[i];
+            }else{
+                v2=prevonedeletion+curr;
+            }
+
+            onedeletion=Math.max(v2,prevnodeletion);
+
+            ans=Math.max(ans,Math.max(nodeletion,onedeletion));
+
+
+        }
+
+        return ans;
+    }
+}
