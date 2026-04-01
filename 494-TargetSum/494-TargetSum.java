09b67bb19e24da36664dd2f78f24d9c7423d5b1f
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-04-01 12:24:51 +0530

    File updated at 1/4/2026, 12:24:51 pm

--- 494-TargetSum/494-TargetSum.java
+++ 494-TargetSum/494-TargetSum.java
@@ -1,19 +1,19 @@
-// Last updated: 3/8/2025, 11:57:47 pm
-class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        return sum(nums,0,target,0,0);
-    }
-    public static int sum(int[] arr,int sum,int target,int idx,int count){
-        if(target ==sum && idx==arr.length){
-            return 1;
-        }
-        if(idx==arr.length)return 0;
-            int x = sum(arr,sum+arr[idx],target,idx+1,count);
-            int y = sum(arr,sum-arr[idx],target,idx+1,count);
-            return x+y;
-
-        
-      
-    }
-
-}
+// Last updated: 1/4/2026, 12:24:51 pm
+1class Solution {
+2    public int findTargetSumWays(int[] nums, int target) {
+3        return sum(nums,0,target,0,0);
+4    }
+5    public static int sum(int[] arr,int sum,int target,int idx,int count){
+6        if(target ==sum && idx==arr.length){
+7            return 1;
+8        }
+9        if(idx==arr.length)return 0;
+10            int x = sum(arr,sum+arr[idx],target,idx+1,count);
+11            int y = sum(arr,sum-arr[idx],target,idx+1,count);
+12            return x+y;
+13
+14        
+15      
+16    }
+17
+18}
