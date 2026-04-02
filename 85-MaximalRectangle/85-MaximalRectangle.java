46c0d046ed9e38e746520a2405572031909b30e7
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-04-02 13:03:45 +0530

    File updated at 2/4/2026, 1:03:43 pm

--- 85-MaximalRectangle/85-MaximalRectangle.java
+++ 85-MaximalRectangle/85-MaximalRectangle.java
@@ -1,4 +1,4 @@
-// Last updated: 2/4/2026, 1:03:25 pm
+// Last updated: 2/4/2026, 1:03:43 pm
 1class Solution {
 2    public int maximalRectangle(char[][] matrix) {
 3        int[][] arr = new int[matrix.length][matrix[0].length];
@@ -16,48 +16,47 @@
 15        }
 16        int ans = 0;
 17        for(int [] x: arr){
-18            int[] height = x.clone();
-19            int res = largestRectangleArea(height);
-20            ans = Math.max(ans,res);
-21        }
-22        return ans;
-23        
-24    }
-25    public static int largestRectangleArea(int[] arr) {
-26		Stack<Integer> st = new Stack<>();
-27		int n = arr.length;
-28		int ans = 0;
-29		
-30		for(int i = 0;i<n; i++) {
-31			while(!st.isEmpty() && arr[st.peek()]>arr[i]) {
-32				int idx = st.pop();
-33				int h = arr[idx];
-34				
-35				int res = 0;
-36				if(st.isEmpty()) {
-37					res = h*i;
-38				}else {
-39					res = (h*(i-st.peek()-1));
-40				}
-41				ans = Math.max(ans, res);
-42				
-43			}
-44			st.push(i);
-45		}
-46		
-47		int r = arr.length;
-48		while(!st.isEmpty()) {
-49			int idx = st.pop();
-50			int h = arr[idx];
-51			
-52			int res = 0;
-53			if(st.isEmpty()) {
-54				res = h*r;
-55			}else {
-56				res = (h*(r-st.peek()-1));
-57			}
-58			ans = Math.max(ans, res);
-59		}
-60		return ans;
-61	}
-62}
+18            int res = largestRectangleArea(x);
+19            ans = Math.max(ans,res);
+20        }
+21        return ans;
+22        
+23    }
+24    public static int largestRectangleArea(int[] arr) {
+25		Stack<Integer> st = new Stack<>();
+26		int n = arr.length;
+27		int ans = 0;
+28		
+29		for(int i = 0;i<n; i++) {
+30			while(!st.isEmpty() && arr[st.peek()]>arr[i]) {
+31				int idx = st.pop();
+32				int h = arr[idx];
+33				
+34				int res = 0;
+35				if(st.isEmpty()) {
+36					res = h*i;
+37				}else {
+38					res = (h*(i-st.peek()-1));
+39				}
+40				ans = Math.max(ans, res);
+41				
+42			}
+43			st.push(i);
+44		}
+45		
+46		int r = arr.length;
+47		while(!st.isEmpty()) {
+48			int idx = st.pop();
+49			int h = arr[idx];
+50			
+51			int res = 0;
+52			if(st.isEmpty()) {
+53				res = h*r;
+54			}else {
+55				res = (h*(r-st.peek()-1));
+56			}
+57			ans = Math.max(ans, res);
+58		}
+59		return ans;
+60	}
+61}
