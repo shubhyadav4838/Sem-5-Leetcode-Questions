606df30767198206be4b5bb910d84e9bd46d29c7
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-04-02 13:03:26 +0530

    File updated at 2/4/2026, 1:03:25 pm

--- 85-MaximalRectangle/85-MaximalRectangle.java
+++ 85-MaximalRectangle/85-MaximalRectangle.java
@@ -1,50 +1,63 @@
-// Last updated: 4/8/2025, 12:00:38 am
-class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        int ans=0;
-		int[] arr = new int[matrix[0].length];
-		for(int i=0;i<matrix.length;i++) {
-			for(int j = 0;j<matrix[0].length;j++) {
-				if(matrix[i][j]=='0') {
-					arr[j]=0;
-				}
-				else {
-					arr[j]++;
-				}
-			}
-			ans = Math.max(ans, LargestHistogram(arr));
-		}
-		return ans;
-    }
-    public static int LargestHistogram(int[] arr) {
-		Stack<Integer> st = new Stack<>();
-		int ans=0;
-		for(int i=0;i<arr.length;i++) {
-			while(!st.isEmpty() && arr[i]<arr[st.peek()]) {
-				int h=arr[st.pop()];
-				int r = i;
-				if(st.isEmpty()) {
-					ans = Math.max(h*r, ans);
-				}else {
-					int l = st.peek();
-					int area = h*(r-l-1);
-					ans= Math.max(area,ans);
-				}
-			}
-			st.push(i);
-		}
-		int r = arr.length;
-		while(!st.isEmpty()) {
-			int h=arr[st.pop()];
-			
-			if(st.isEmpty()) {
-				ans = Math.max(h*r, ans);
-			}else {
-				int l = st.peek();
-				int area = h*(r-l-1);
-				ans= Math.max(area,ans);
-			}
-		}
-		return ans;
-	}
-}
+// Last updated: 2/4/2026, 1:03:25 pm
+1class Solution {
+2    public int maximalRectangle(char[][] matrix) {
+3        int[][] arr = new int[matrix.length][matrix[0].length];
+4        for(int i = 0;i<arr.length;i++){
+5            for(int j = 0; j<arr[0].length; j++){
+6                if(matrix[i][j] =='1')arr[i][j] = 1;
+7            }
+8        }
+9        for(int i = 1; i<arr.length; i++){
+10            for(int j = 0; j<arr[0].length; j++){
+11                if(arr[i][j]!=0){
+12                    arr[i][j] += arr[i-1][j];
+13                }
+14            }
+15        }
+16        int ans = 0;
+17        for(int [] x: arr){
+18            int[] height = x.clone();
+19            int res = largestRectangleArea(height);
+20            ans = Math.max(ans,res);
+21        }
+22        return ans;
+23        
+24    }
+25    public static int largestRectangleArea(int[] arr) {
+26		Stack<Integer> st = new Stack<>();
+27		int n = arr.length;
+28		int ans = 0;
+29		
+30		for(int i = 0;i<n; i++) {
+31			while(!st.isEmpty() && arr[st.peek()]>arr[i]) {
+32				int idx = st.pop();
+33				int h = arr[idx];
+34				
+35				int res = 0;
+36				if(st.isEmpty()) {
+37					res = h*i;
+38				}else {
+39					res = (h*(i-st.peek()-1));
+40				}
+41				ans = Math.max(ans, res);
+42				
+43			}
+44			st.push(i);
+45		}
+46		
+47		int r = arr.length;
+48		while(!st.isEmpty()) {
+49			int idx = st.pop();
+50			int h = arr[idx];
+51			
+52			int res = 0;
+53			if(st.isEmpty()) {
+54				res = h*r;
+55			}else {
+56				res = (h*(r-st.peek()-1));
+57			}
+58			ans = Math.max(ans, res);
+59		}
+60		return ans;
+61	}
+62}
