bb54e318baed70522077623b9efba08c6652c3ad
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-07-15 10:02:21 +0530

    File updated at 15/7/2026, 10:02:19 am

--- 3336-FindtheNumberofSubsequencesWithEqualGCD/3336-FindtheNumberofSubsequencesWithEqualGCD.java
+++ 3336-FindtheNumberofSubsequencesWithEqualGCD/3336-FindtheNumberofSubsequencesWithEqualGCD.java
@@ -1,37 +1,63 @@
-// Last updated: 15/7/2026, 9:58:17 am
+// Last updated: 15/7/2026, 10:02:19 am
 1class Solution {
 2    public static int subsequencePairCount(int[] arr) {
-3		Integer[][][] dp = new Integer[arr.length][201][201];
-4		return solve(0,0,0,arr,dp);
-5
-6	}
-7	static int MOD = (int)1e9 + 7;
-8	
-9	public static int solve(int seqa, int seqb, int idx, int[] arr, Integer[][][] dp) {
-10		if (idx >= arr.length) {
-11			return (seqa != 0 && seqa == seqb ? 1 : 0);
-12		}
-13
-14		if (dp[idx][seqa][seqb] != null) {
-15			return dp[idx][seqa][seqb];
-16		}
-17
-18		long ans = 0;
-19		ans += solve(seqa, seqb, idx + 1, arr, dp) % MOD;
-20
-21		ans += solve(gcd(seqa, arr[idx]), seqb, idx + 1, arr, dp) % MOD;
-22		ans += solve(seqa, gcd(seqb, arr[idx]), idx + 1, arr, dp) % MOD;
-23
-24		return dp[idx][seqa][seqb] = (int) (ans % MOD);
-25
-26	}
-27	
-28	private static int gcd(int a, int b) {
-29        while (b != 0) {
-30            int t = a % b;
-31            a = b;
-32            b = t;
-33        }
-34        return a;
-35    }
-36}
+3		return bottomUp(arr);
+4
+5	}
+6	static int MOD = (int)1e9 + 7;
+7	
+8	public static int bottomUp(int[] arr) {
+9		
+10        int maxVal = 0;
+11
+12        int n = arr.length;
+13        for(int x:arr){
+14            maxVal = Math.max(x,maxVal);
+15        }
+16
+17        int[][][] dp = new int[arr.length + 1][maxVal+1][maxVal+1];
+18		
+19		
+20		// Base case
+21		for(int seqa = 0; seqa<=maxVal;seqa++) {
+22			for(int seqb = 0; seqb<=maxVal; seqb++) {
+23				boolean notNull = (seqa!=0 && seqb!=0);
+24				boolean equal = (seqa==seqb);
+25				if(notNull && equal) {
+26					dp[n][seqa][seqb] = 1;
+27				}else {
+28					dp[n][seqa][seqb] = 0;
+29				}
+30			}
+31			
+32		}
+33
+34		for (int idx = arr.length - 1; idx >= 0; idx--) {
+35			
+36			for (int seqa = maxVal; seqa >= 0; seqa--) {
+37				for (int seqb = maxVal; seqb >=0; seqb--) {
+38					
+39					int skip = dp[idx+1][seqa][seqb];
+40					int takea = dp[idx+1][gcd(seqa,arr[idx])][seqb];
+41					int takeb = dp[idx+1][seqa][gcd(seqb,arr[idx])];
+42					
+43					long ans = (long) skip + takea + takeb;
+44					
+45					dp[idx][seqa][seqb] = (int)(ans%MOD);
+46					
+47				}
+48			}
+49		}
+50		
+51		return dp[0][0][0];
+52	}
+53	
+54	private static int gcd(int a, int b) {
+55        while (b != 0) {
+56            int t = a % b;
+57            a = b;
+58            b = t;
+59        }
+60        return a;
+61    }
+62}
