178648ad003cec155797077682ba2d27053bdb7b
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-07-15 09:58:19 +0530

    File updated at 15/7/2026, 9:58:17 am

--- 3336-FindtheNumberofSubsequencesWithEqualGCD/3336-FindtheNumberofSubsequencesWithEqualGCD.java
+++ 3336-FindtheNumberofSubsequencesWithEqualGCD/3336-FindtheNumberofSubsequencesWithEqualGCD.java
@@ -1,4 +1,4 @@
-// Last updated: 15/7/2026, 9:46:12 am
+// Last updated: 15/7/2026, 9:58:17 am
 1class Solution {
 2    public static int subsequencePairCount(int[] arr) {
 3		Integer[][][] dp = new Integer[arr.length][201][201];
@@ -7,34 +7,31 @@
 6	}
 7	static int MOD = (int)1e9 + 7;
 8	
-9	public static int solve(int seqa,int seqb, int idx,int[] arr,Integer[][][] dp) {
-10		if(idx>=arr.length) {
-11			return (seqa!=0 && seqa==seqb ? 1:0);
+9	public static int solve(int seqa, int seqb, int idx, int[] arr, Integer[][][] dp) {
+10		if (idx >= arr.length) {
+11			return (seqa != 0 && seqa == seqb ? 1 : 0);
 12		}
-13		
-14		if(dp[idx][seqa][seqb]!=null) {
+13
+14		if (dp[idx][seqa][seqb] != null) {
 15			return dp[idx][seqa][seqb];
 16		}
-17		
+17
 18		long ans = 0;
-19		ans += solve(seqa, seqb, idx+1, arr, dp)%MOD;
-20		
-21		int vala = gcd(seqa,arr[idx]);
-22		int valb = gcd(seqb,arr[idx]);
-23		
-24		ans += solve(vala, seqb, idx+1, arr, dp)%MOD;
-25		ans += solve(seqa, valb, idx+1, arr, dp)%MOD;
-26		
-27		return dp[idx][seqa][seqb] = (int)(ans%MOD);
-28		
-29	}
-30	
-31	private static int gcd(int a, int b) {
-32        while (b != 0) {
-33            int t = a % b;
-34            a = b;
-35            b = t;
-36        }
-37        return a;
-38    }
-39}
+19		ans += solve(seqa, seqb, idx + 1, arr, dp) % MOD;
+20
+21		ans += solve(gcd(seqa, arr[idx]), seqb, idx + 1, arr, dp) % MOD;
+22		ans += solve(seqa, gcd(seqb, arr[idx]), idx + 1, arr, dp) % MOD;
+23
+24		return dp[idx][seqa][seqb] = (int) (ans % MOD);
+25
+26	}
+27	
+28	private static int gcd(int a, int b) {
+29        while (b != 0) {
+30            int t = a % b;
+31            a = b;
+32            b = t;
+33        }
+34        return a;
+35    }
+36}
