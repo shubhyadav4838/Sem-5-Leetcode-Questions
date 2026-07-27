39249b23e02c5b23bc528ec4ed3f3199621f6f3e
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-07-27 11:46:08 +0530

    Create 1218-LowestCommonAncestorOfDeepestLeaves

---
+++ 1218-LowestCommonAncestorOfDeepestLeaves/1218-LowestCommonAncestorOfDeepestLeaves.java
@@ -0,0 +1,30 @@
+// Last updated: 27/7/2026, 11:46:08 am
+class Solution {
+    int max = 0;
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        if (root == null) return null;
+        max = 0;
+        calcMax(root, 1);          
+        return deep(root, 1);     
+    }
+
+    private void calcMax(TreeNode node, int curr) {
+        if (node == null) return;
+        if (curr > max) max = curr;
+        calcMax(node.left, curr + 1);
+        calcMax(node.right, curr + 1);
+    }
+
+    public TreeNode deep(TreeNode root, int curr) {
+        if (root == null) return null;
+
+        if (curr == max) return root;  
+
+        TreeNode left = deep(root.left, curr + 1);
+        TreeNode right = deep(root.right, curr + 1);
+
+        if (left != null && right != null) return root;
+        return (left != null) ? left : right;   
+    }
+}
