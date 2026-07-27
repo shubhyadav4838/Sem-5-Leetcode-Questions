ab245213deb5fa46de1b4d12fe347f0d2cf74eb2
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-07-27 11:32:24 +0530

    File updated at 27/7/2026, 11:32:24 am

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,22 +1,31 @@
-// Last updated: 29/3/2026, 1:06:15 pm
+// Last updated: 27/7/2026, 11:32:24 am
 1class Solution {
-2    public static int longestCommonSubsequence(String text1, String text2) {
-3		int[] dp = new int[text2.length() + 1];
-4        int n = text1.length();
-5		
-6		for (int i = 1; i <= text1.length(); i++) {
-7			for (int j = text2.length(); j >0; j--) {
-8				if (text1.charAt(i-1 ) == text2.charAt(j - 1)) {
-9					dp[j] =  dp[j-1]+1;
-10				} 
-11			}
-12            for (int j = 1; j <= text2.length(); j++) {
-13				if (text1.charAt(i - 1) != text2.charAt(j - 1)) {
-14					dp[j] = Math.max(dp[j], dp[j - 1]);
-15				} 
-16			}
-17            
+2    public static int longestCommonSubsequence(String s1, String s2) {
+3		int[][] dp = new int[1001][1001];
+4		for(int[] x:dp) {
+5			Arrays.fill(x, -1);
+6		}
+7		int ans = solve(s1,s2,0,0,dp);
+8		return ans;
+9	}
+10
+11	private static int solve(String s1, String s2, int i, int j, int[][] dp) {
+12		// TODO Auto-generated method stub
+13		if(i>=s1.length() || j>=s2.length())
+14		return 0;
+15		
+16		if(dp[i][j]!=-1) {
+17			return dp[i][j];
 18		}
-19		return dp[text2.length()];
-20	}
-21}
+19		int ans = 0;
+20		if(s1.charAt(i)==s2.charAt(j)) {
+21			ans = solve(s1,s2,i+1,j+1,dp) + 1;
+22			
+23		}
+24		int b = solve(s1,s2,i+1,j,dp);
+25		int c = solve(s1,s2,i,j+1,dp);
+26		
+27		return dp[i][j] = Math.max(ans, Math.max(b, c));
+28		
+29	}
+30}
