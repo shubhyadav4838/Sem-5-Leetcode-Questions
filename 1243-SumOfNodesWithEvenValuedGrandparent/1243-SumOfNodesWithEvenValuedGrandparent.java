57af90ab4a1756dd6a2ae979815f0f6100fe72be
Shubh Yadav <155947331+shubhyadav4838@users.noreply.github.com>
2026-07-27 11:46:02 +0530

    Create 1243-SumOfNodesWithEvenValuedGrandparent

---
+++ 1243-SumOfNodesWithEvenValuedGrandparent/1243-SumOfNodesWithEvenValuedGrandparent.java
@@ -0,0 +1,34 @@
+// Last updated: 27/7/2026, 11:46:02 am
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public int sumEvenGrandparent(TreeNode root) {
+        TreeNode dummy = root;
+        ben10(root,false,false);
+        return sum;
+    }
+    public void ben10(TreeNode root, boolean gp, boolean p){
+        if(root==null) return;
+        if(gp){
+            sum+=root.val;
+        }
+        ben10(root.left,p,root.val%2==0);
+        ben10(root.right,p,root.val%2==0);
+
+
+    }
+}
